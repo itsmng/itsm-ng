e2ea176919b2ac679aab7013584890f9775d2490
ITSM Dev Team <devteam@itsm-ng.org>
2022-03-08 08:47:49 +0000

    Update title setup and remove telemetry

--- install/update.php
+++ install/update.php
@@ -552,7 +552,7 @@ echo "<head>";
 echo "<meta charset='utf-8'>";
 echo "<meta http-equiv='Content-Script-Type' content='text/javascript'>";
 echo "<meta http-equiv='Content-Style-Type' content='text/css'>";
-echo "<title>Setup GLPI</title>";
+echo "<title>Setup ITSM-NG</title>";
 //JS
 echo Html::script("public/lib/base.js");
 // CSS
@@ -563,7 +563,7 @@ echo "<body>";
 echo "<div id='principal'>";
 echo "<div id='bloc'>";
 echo "<div id='logo_bloc'></div>";
-echo "<h2>GLPI SETUP</h2>";
+echo "<h2>ITSM-NG SETUP</h2>";
 echo "<br><h3>".__('Upgrade')."</h3>";
 
 // step 1    avec bouton de confirmation
@@ -640,13 +640,6 @@ if (empty($_POST["continuer"]) && empty($_POST["from_update"])) {
                default:
                   echo "<form action='".$CFG_GLPI["root_doc"]."/install/update.php' method='post'>";
                   echo "<input type='hidden' name='update_end' value='1'/>";
-
-                  if (!Telemetry::isEnabled()) {
-                     echo "<hr />";
-                     $_SESSION['telemetry_from_install'] = true;
-                     echo Telemetry::showTelemetry();
-                  }
-                  echo Telemetry::showReference();
 
                   echo "<p class='submit'><input type='submit' name='submit' class='submit' value='".
                            __('Use GLPI')."'></p>";
