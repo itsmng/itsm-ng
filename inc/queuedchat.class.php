4b543d0332ed20b8f74e7bd05e3716b8079c465c
Djily SARR <djilysarr275@gmail.com>
2022-08-26 13:57:40 +0000

    Queued class and form

---
+++ inc/queuedchat.class.php
@@ -0,0 +1,523 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ * Copyright (C) 2015-2022 Teclib' and contributors.
+ *
+ * http://glpi-project.org
+ *
+ * based on GLPI - Gestionnaire Libre de Parc Informatique
+ * Copyright (C) 2003-2014 by the INDEPNET Development Team.
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * GLPI is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GLPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------
+ */
+
+if (!defined('GLPI_ROOT')) {
+    die("Sorry. You can't access this file directly");
+}
+
+
+/** QueuedChat class
+ *
+ * @since 0.85
+ **/
+class QueuedChat extends CommonDBTM
+{
+
+    static $rightname = 'queuedchat';
+
+
+    static function getTypeName($nb = 0)
+    {
+        return __('Chat queue');
+    }
+
+
+    static function canCreate()
+    {
+        // Everybody can create : human and cron
+        return Session::getLoginUserID(false);
+    }
+
+    static function getForbiddenActionsForMenu()
+    {
+        return ['add'];
+    }
+
+
+    function getForbiddenStandardMassiveAction()
+    {
+
+        $forbidden   = parent::getForbiddenStandardMassiveAction();
+        $forbidden[] = 'update';
+        return $forbidden;
+    }
+
+    /**
+     * @see CommonDBTM::getSpecificMassiveActions()
+     **/
+    function getSpecificMassiveActions($checkitem = null, $is_deleted = false)
+    {
+
+        $isadmin = static::canUpdate();
+        $actions = parent::getSpecificMassiveActions($checkitem);
+
+        if ($isadmin && !$is_deleted) {
+            $actions[__CLASS__ . MassiveAction::CLASS_ACTION_SEPARATOR . 'sendchat'] = _x('button', 'Send');
+        }
+
+        return $actions;
+    }
+
+    /**
+     * @see CommonDBTM::processMassiveActionsForOneItemtype()
+     **/
+    static function processMassiveActionsForOneItemtype(
+        MassiveAction $ma,
+        CommonDBTM $item,
+        array $ids
+    ) {
+        switch ($ma->getAction()) {
+            case 'sendchat':
+                foreach ($ids as $id) {
+                    if ($item->canEdit($id)) {
+                        if ($item->sendById($id)) {
+                            $ma->itemDone($item->getType(), $id, MassiveAction::ACTION_OK);
+                        } else {
+                            $ma->itemDone($item->getType(), $id, MassiveAction::ACTION_KO);
+                        }
+                    } else {
+                        $ma->itemDone($item->getType(), $id, MassiveAction::ACTION_NORIGHT);
+                    }
+                }
+                return;
+        }
+        parent::processMassiveActionsForOneItemtype($ma, $item, $ids);
+    }
+
+    /**
+     * @param $field
+     * @param $values
+     * @param $options   array
+     **/
+    static function getSpecificValueToDisplay($field, $values, array $options = [])
+    {
+
+        if (!is_array($values)) {
+            $values = [$field => $values];
+        }
+        switch ($field) {
+            case 'headers':
+                $values[$field] = importArrayFromDB($values[$field]);
+                $out = '';
+                if (is_array($values[$field]) && count($values[$field])) {
+                    foreach ($values[$field] as $key => $val) {
+                        $out .= $key . ': ' . $val . '<br>';
+                    }
+                }
+                return $out;
+                break;
+            case 'mode':
+                $out = Notification_NotificationTemplate::getMode($values[$field])['label'];
+                return $out;
+                break;
+        }
+        return parent::getSpecificValueToDisplay($field, $values, $options);
+    }
+
+
+    static function getSpecificValueToSelect($field, $name = '', $values = '', array $options = [])
+    {
+        if (!is_array($values)) {
+            $values = [$field => $values];
+        }
+        $options['display'] = false;
+
+        switch ($field) {
+            case 'mode':
+                $options['name']  = $name;
+                $options['value'] = $values[$field];
+                return Notification_NotificationTemplate::dropdownMode($options);
+        }
+        return parent::getSpecificValueToSelect($field, $name, $values, $options);
+    }
+
+
+    /**
+     * Send chat in queue
+     *
+     * @param integer $ID Id
+     *
+     * @return boolean
+     */
+    public function sendById($ID)
+    {
+        if ($this->getFromDB($ID)) {
+            $mode = $this->getField('mode');
+            $eventclass = 'NotificationEvent' . ucfirst($mode);
+            $conf = Notification_NotificationTemplate::getMode($mode);
+            if ($conf['from'] != 'core') {
+                $eventclass = 'Plugin' . ucfirst($conf['from']) . $eventclass;
+            }
+
+            return $eventclass::send([$this->fields]);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Give cron information
+     *
+     * @param $name : task's name
+     *
+     * @return array of information
+     **/
+    static function cronInfo($name)
+    {
+
+        switch ($name) {
+            case 'queuedchat':
+                return [
+                    'description' => __('Send chat in queue'),
+                    'parameter'   => __('Maximum chat to send at once')
+                ];
+
+            case 'queuedchatclean':
+                return [
+                    'description' => __('Clean chat queue'),
+                    'parameter'   => __('Days to keep sent chat')
+                ];
+        }
+        return [];
+    }
+
+
+
+    /**
+     * Get pending chat in queue
+     *
+     * @param string  $send_time   Maximum sent_time
+     * @param integer $limit       Query limit clause
+     * @param array   $limit_modes Modes to limit to
+     * @param array   $extra_where Extra params to add to the where clause
+     *
+     * @return array
+     */
+    static public function getPendings($send_time = null, $limit = 20, $limit_modes = null, $extra_where = [])
+    {
+        global $DB, $CFG_GLPI;
+
+        if ($send_time === null) {
+            $send_time = date('Y-m-d H:i:s');
+        }
+
+        $base_query = [
+            'FROM'   => self::getTable(),
+            'WHERE'  => [
+                'is_deleted'   => 0,
+                'mode'         => 'TOFILL',
+                'send_time'    => ['<', $send_time],
+            ] +  $extra_where,
+            'ORDER'  => 'send_time ASC',
+            'START'  => 0,
+            'LIMIT'  => $limit
+        ];
+
+        $pendings = [];
+        $modes = Notification_NotificationTemplate::getModes();
+        foreach ($modes as $mode => $conf) {
+            $eventclass = 'NotificationEvent' . ucfirst($mode);
+            if ($conf['from'] != 'core') {
+                $eventclass = 'Plugin' . ucfirst($conf['from']) . $eventclass;
+            }
+
+            if (
+                $limit_modes !== null && !in_array($mode, $limit_modes)
+                || !$CFG_GLPI['notifications_' . $mode]
+                || !$eventclass::canCron()
+            ) {
+                //mode is not in limits, is disabled, or cannot be called from cron, passing
+                continue;
+            }
+
+            $query = $base_query;
+            $query['WHERE']['mode'] = $mode;
+
+            $iterator = $DB->request($query);
+            if ($iterator->numRows() > 0) {
+                $pendings[$mode] = [];
+                while ($row = $iterator->next()) {
+                    $pendings[$mode][] = $row;
+                }
+            }
+        }
+
+        return $pendings;
+    }
+
+
+    /**
+     * Cron action on chat queue: send chat in queue
+     *
+     * @param CommonDBTM $task for log (default NULL)
+     *
+     * @return integer either 0 or 1
+     **/
+    static function cronQueuedChat($task = null)
+    {
+        if (!Notification_NotificationTemplate::hasActiveMode()) {
+            return 0;
+        }
+        $cron_status = 0;
+
+        // Send notifications at least 1 minute after adding in queue to be sure that process on it is finished
+        $send_time = date("Y-m-d H:i:s", strtotime("+1 minutes"));
+
+        $pendings = self::getPendings(
+            $send_time,
+            $task->fields['param']
+        );
+
+        foreach ($pendings as $mode => $data) {
+            $eventclass = 'NotificationEvent' . ucfirst($mode);
+            $conf = Notification_NotificationTemplate::getMode($mode);
+            if ($conf['from'] != 'core') {
+                $eventclass = 'Plugin' . ucfirst($conf['from']) . $eventclass;
+            }
+
+            $result = $eventclass::send($data);
+            if ($result !== false) {
+                $cron_status = 1;
+                if (!is_null($task)) {
+                    $task->addVolume($result);
+                }
+            }
+        }
+
+        return $cron_status;
+    }
+
+
+    /**
+     * Cron action on queued notification: clean notification queue
+     *
+     * @param CommonDBTM $task for log (default NULL)
+     *
+     * @return integer either 0 or 1
+     **/
+    static function cronQueuedChatClean($task = null)
+    {
+        global $DB;
+
+        $vol = 0;
+
+        // Expire mails in queue
+        if ($task->fields['param'] > 0) {
+            $secs      = $task->fields['param'] * DAY_TIMESTAMP;
+            $send_time = date("U") - $secs;
+            $DB->delete(
+                self::getTable(),
+                [
+                    'is_deleted'   => 1,
+                    new \QueryExpression('(UNIX_TIMESTAMP(' . $DB->quoteName('send_time') . ') < ' . $DB->quoteValue($send_time) . ')')
+                ]
+            );
+            $vol = $DB->affectedRows();
+        }
+
+        $task->setVolume($vol);
+        return ($vol > 0 ? 1 : 0);
+    }
+
+
+
+    /**
+     * Force sending all chat in queue for a specific item
+     *
+     * @param string  $itemtype item type
+     * @param integer $items_id id of the item
+     *
+     * @return void
+     **/
+    static function forceSendFor($itemtype, $items_id)
+    {
+        if (
+            !empty($itemtype)
+            && !empty($items_id)
+        ) {
+            $pendings = self::getPendings(
+                null,
+                1,
+                null,
+                [
+                    'itemtype'  => $itemtype,
+                    'items_id'  => $items_id
+                ]
+            );
+
+            foreach ($pendings as $mode => $data) {
+                $eventclass = Notification_NotificationTemplate::getModeClass($mode, 'event');
+                $eventclass::send($data);
+            }
+        }
+    }
+
+    /**
+     * Print the queued chat form
+     *
+     * @param integer $ID      ID of the item
+     * @param array   $options Options
+     *
+     * @return true if displayed  false if item not found or not right to display
+     **/
+    function showForm($ID, $options = [])
+    {
+        if (!Session::haveRight("queuedchat", READ)) {
+            return false;
+        }
+
+        $this->check($ID, READ);
+        $options['canedit'] = false;
+
+        $this->showFormHeader($options);
+        echo "<tr class='tab_bg_1'>";
+        echo "<td>" . _n('Type', 'Types', 1) . "</td>";
+
+        echo "<td>";
+        if (!($item = getItemForItemtype($this->fields['itemtype']))) {
+            echo NOT_AVAILABLE;
+            echo "</td>";
+            echo "<td>" . _n('Item', 'Items', 1) . "</td>";
+            echo "<td>";
+            echo NOT_AVAILABLE;
+        } else if ($item instanceof CommonDBTM) {
+            echo $item->getType();
+            $item->getFromDB($this->fields['items_id']);
+            echo "</td>";
+            echo "<td>" . _n('Item', 'Items', 1) . "</td>";
+            echo "<td>";
+            echo $item->getLink();
+        } else {
+            echo get_class($item);
+            echo "</td><td></td>";
+        }
+        echo "</tr>";
+
+        echo "<tr class='tab_bg_1'>";
+        echo "<td>" . _n('Notification template', 'Notification templates', 1) . "</td>";
+        echo "<td>";
+        echo Dropdown::getDropdownName(
+            'glpi_notificationtemplates',
+            $this->fields['notificationtemplates_id']
+        );
+        echo "</td>";
+        echo "<td>&nbsp;</td>";
+        echo "<td>&nbsp;</td>";
+        echo "</tr>";
+
+        echo "<tr class='tab_bg_1'>";
+        echo "<td>" . __('Creation date') . "</td>";
+        echo "<td>";
+        echo Html::convDateTime($this->fields['create_time']);
+        echo "</td><td>" . __('Expected send date') . "</td>";
+        echo "<td>" . Html::convDateTime($this->fields['send_time']) . "</td>";
+        echo "</tr>";
+
+        echo "<tr class='tab_bg_1'>";
+        echo "<td>" . __('Send date') . "</td>";
+        echo "<td>" . Html::convDateTime($this->fields['sent_time']) . "</td>";
+        echo "<td>" . __('Number of tries of sent') . "</td>";
+        echo "<td>" . $this->fields['sent_try'] . "</td>";
+        echo "</tr>";
+
+        echo "<tr class='tab_bg_1'>";
+        echo "<td>" . __('Ticket name') . "</td>";
+        echo "<td>" . $this->fields['ticketTitle'] . "</td>";
+        echo "</tr>";
+
+        echo "<tr class='tab_bg_1'>";
+        echo "<td>" . __('Ticket ID') . "</td>";
+        echo "<td>" . $this->fields['ticketId'] . "</td>";
+        echo "</tr>";
+
+        echo "<tr class='tab_bg_1'>";
+        echo "<td>" . __('Server name') . "</td>";
+        echo "<td>" . $this->fields['serverName'] . "</td>";
+        echo "</tr>";
+
+        echo "<tr class='tab_bg_1'>";
+        echo "<td>" . __('Rocket Hook Url') . "</td>";
+        echo "<td>" . $this->fields['rocketHookUrl'] . "</td>";
+        echo "</tr>";
+
+        echo "<tr class='tab_bg_1 top' >";
+        echo "<td colspan='2' class='queuechat_preview'>" . self::cleanHtml($this->fields['ticketTitle']) . "</td>";
+        echo "<td colspan='2'>" . nl2br($this->fields['body_text'], false) . "</td>";
+        echo "</tr>";
+
+        $this->showFormButtons($options);
+
+        return true;
+    }
+
+    /**
+     * @since 0.85
+     *
+     * @param $string
+     **/
+    static function cleanHtml($string)
+    {
+
+        $begin_strip     = -1;
+        $end_strip       = -1;
+        $begin_match     = "/<body>/";
+        $end_match       = "/<\/body>/";
+        $content         = explode("\n", $string);
+        $newstring       = '';
+        foreach ($content as $ID => $val) {
+            // Get last tag for end
+            if ($begin_strip >= 0) {
+                if (preg_match($end_match, $val)) {
+                    $end_strip = $ID;
+                    continue;
+                }
+            }
+            if (($begin_strip >= 0) && ($end_strip < 0)) {
+                $newstring .= $val;
+            }
+            // Get first tag for begin
+            if ($begin_strip < 0) {
+                if (preg_match($begin_match, $val)) {
+                    $begin_strip = $ID;
+                }
+            }
+        }
+        return nl2br($newstring, false);
+    }
+
+
+    static function getIcon()
+    {
+        return "far fa-list-alt";
+    }
+}
