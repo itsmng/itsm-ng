9cfc718f246746d0fe4a2381d29ce944a1c73b88
Rudy Laurent <rudy@factorfx.com>
2022-09-20 17:51:18 +0200

    Notification chat can use models

--- inc/notificationchat.class.php
+++ inc/notificationchat.class.php
@@ -86,11 +86,12 @@ class NotificationChat implements NotificationInterface
 
         $data['serverName']                           = $_SERVER['SERVER_NAME'] . $_SESSION['glpiroot'];
 
-        $entity = new Entity();
+        /* $entity = new Entity();
         if ($entity->getFromDB($options['_entities_id'])) {
             $entName = $entity->getField('name');
             $data['entName']                          = $entName;
-        }
+        } */
+        $data['entName'] = $options['content_text'];
 
         $ticket = new Ticket();
         if ($ticket->getFromDB($options['_items_id'])) {
--- inc/notificationchatconfig.class.php
+++ inc/notificationchatconfig.class.php
@@ -28,13 +28,73 @@ class NotificationChatConfig extends CommonDBTM
         $rocketHookUrl = $rocketHookUrl;
 
         $jsonStructure = '{"alias":"test","text":"%s"}';
-        $textStructure = "Client : **%s** \n Le ticket numéro %s à été créé : **%s** \n Accédez au ticket en [cliquant ici](http://%s/front/ticket.form.php?id=%s)";
+        $textStructure = "Entite : **%s** \n Le ticket numéro %s à été créé : **%s** \n Accédez au ticket en [cliquant ici](http://%s/front/ticket.form.php?id=%s)";
 
         $data = array(
             'alias' => 'test',
             'text' => ''
         );
         $data["text"] = sprintf($textStructure, $entName, $ticketId, $ticketTitle, $glpiUrl, $ticketId);
+
+        $payload = json_encode($data);
+
+        $ch = curl_init($rocketHookUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+
+        // Set HTTP Header for POST request 
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($payload)
+            )
+        );
+
+        // Submit the POST request
+        $result = curl_exec($ch);
+        if ($result) {
+            Toolbox::logInFile(
+                "chat",
+                sprintf(
+                    __('Rocket chat: the chat %s was sent'),
+                    $ticketTitle
+                ) . "\n"
+            );
+        } else {
+            Toolbox::logInFile(
+                "chat-error",
+                sprintf(
+                    __('Fatal-error: the chat %s was not send to rocket chat'),
+                    $ticketTitle
+                ) . "\n"
+            );
+        }
+
+        // Close cURL session handle
+        curl_close($ch);
+    }
+
+    public function sendRocketNotificationNew($ticketTitle, $ticketId, $entName, $serverName, $rocketHookUrl)
+    {
+        $glpiUrl = $serverName;
+        $entName = $entName;
+        $ticketId = $ticketId;
+        $ticketTitle = $ticketTitle;
+        $rocketHookUrl = $rocketHookUrl;
+
+        $jsonStructure = '{"alias":"test","text":"%s"}';
+        $textStructure = $entName;
+
+        $data = array(
+            'alias' => 'test',
+            'text' => ''
+        );
+        /* $data["text"] = sprintf($textStructure, $entName, $ticketId, $ticketTitle, $glpiUrl, $ticketId); */
+        $data["text"] = $entName;
 
         $payload = json_encode($data);
 
