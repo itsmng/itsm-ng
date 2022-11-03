266ef26d46a22d32d888962b46bbba16f93611d7
ITSM Dev Team <devteam@itsm-ng.org>
2022-11-03 08:22:07 +0000

    Add new user columns in migration 130

--- install/itsm_update/update_120_130.php
+++ install/itsm_update/update_120_130.php
@@ -36,108 +36,121 @@
  * @return bool for success (will die for most error)
  **/
 function update120to130() {
-   /** @global Migration $migration */
-   global $DB, $migration;
+    /** @global Migration $migration */
+    global $DB, $migration;
 
-   $current_config   = Config::getConfigurationValues('core');
-   $updateresult     = true;
-   $ADDTODISPLAYPREF = [];
+    $current_config   = Config::getConfigurationValues('core');
+    $updateresult     = true;
+    $ADDTODISPLAYPREF = [];
 
-   $migration->displayTitle(sprintf(__('Update to %s'), '1.3.0'));
+    $migration->displayTitle(sprintf(__('Update to %s'), '1.3.0'));
 
 
-   /** Create new table for chat config */
-   if (!$DB->tableExists("glpi_notificationchatconfigs")) {
-    $config = "CREATE TABLE `glpi_notificationchatconfigs` (
-        `id` int(11) NOT NULL AUTO_INCREMENT,
-        `hookurl` varchar(255) DEFAULT NULL,
-        `chat` varchar(255) DEFAULT NULL,
-        `type` varchar(255) DEFAULT NULL,
-        `value` varchar(255) DEFAULT NULL,
-        PRIMARY KEY (`id`)
-      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    /** Create new table for chat config */
+    if (!$DB->tableExists("glpi_notificationchatconfigs")) {
+        $config = "CREATE TABLE `glpi_notificationchatconfigs` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `hookurl` varchar(255) DEFAULT NULL,
+            `chat` varchar(255) DEFAULT NULL,
+            `type` varchar(255) DEFAULT NULL,
+            `value` varchar(255) DEFAULT NULL,
+            PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
         $DB->queryOrDie($config, "erreur lors de la création de la table de configuration ".$DB->error());
     }
 
     /** Create new table for notification chat queue */
     if (!$DB->tableExists("glpi_queuedchats")) {
-     $config = "CREATE TABLE `glpi_queuedchats` (
-        `id` int(11) NOT NULL AUTO_INCREMENT,
-        `itemtype` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-        `items_id` int(11) NOT NULL DEFAULT '0',
-        `notificationtemplates_id` int(11) NOT NULL DEFAULT '0',
-        `entities_id` int(11) NOT NULL DEFAULT '0',
-        `locations_id` int(11) NOT NULL DEFAULT '0',
-        `groups_id` int(11) NOT NULL DEFAULT '0',
-        `itilcategories_id` int(11) NOT NULL DEFAULT '0',
-        `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
-        `sent_try` int(11) NOT NULL DEFAULT '0',
-        `create_time` timestamp NULL DEFAULT NULL,
-        `send_time` timestamp NULL DEFAULT NULL,
-        `sent_time` timestamp NULL DEFAULT NULL,
-        `entName` text COLLATE utf8_unicode_ci,
-        `ticketTitle` text COLLATE utf8_unicode_ci,
-        `completName` text COLLATE utf8_unicode_ci,
-        `serverName` text COLLATE utf8_unicode_ci,
-        `hookurl` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
-        `mode` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT 'See Notification_NotificationTemplate::MODE_* constants',
-        PRIMARY KEY (`id`),
-        KEY `item` (`itemtype`,`items_id`,`notificationtemplates_id`),
-        KEY `is_deleted` (`is_deleted`),
-        KEY `entities_id` (`entities_id`),
-        KEY `sent_try` (`sent_try`),
-        KEY `create_time` (`create_time`),
-        KEY `send_time` (`send_time`),
-        KEY `sent_time` (`sent_time`),
-        KEY `mode` (`mode`)
-      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
-         $DB->queryOrDie($config, "erreur lors de la création de la table de configuration ".$DB->error());
-     }
+        $config = "CREATE TABLE `glpi_queuedchats` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `itemtype` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+            `items_id` int(11) NOT NULL DEFAULT '0',
+            `notificationtemplates_id` int(11) NOT NULL DEFAULT '0',
+            `entities_id` int(11) NOT NULL DEFAULT '0',
+            `locations_id` int(11) NOT NULL DEFAULT '0',
+            `groups_id` int(11) NOT NULL DEFAULT '0',
+            `itilcategories_id` int(11) NOT NULL DEFAULT '0',
+            `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+            `sent_try` int(11) NOT NULL DEFAULT '0',
+            `create_time` timestamp NULL DEFAULT NULL,
+            `send_time` timestamp NULL DEFAULT NULL,
+            `sent_time` timestamp NULL DEFAULT NULL,
+            `entName` text COLLATE utf8_unicode_ci,
+            `ticketTitle` text COLLATE utf8_unicode_ci,
+            `completName` text COLLATE utf8_unicode_ci,
+            `serverName` text COLLATE utf8_unicode_ci,
+            `hookurl` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+            `mode` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT 'See Notification_NotificationTemplate::MODE_* constants',
+            PRIMARY KEY (`id`),
+            KEY `item` (`itemtype`,`items_id`,`notificationtemplates_id`),
+            KEY `is_deleted` (`is_deleted`),
+            KEY `entities_id` (`entities_id`),
+            KEY `sent_try` (`sent_try`),
+            KEY `create_time` (`create_time`),
+            KEY `send_time` (`send_time`),
+            KEY `sent_time` (`sent_time`),
+            KEY `mode` (`mode`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+        $DB->queryOrDie($config, "erreur lors de la création de la table de configuration ".$DB->error());
+    }
 
     /** Create new table for Open ID connect's config */
     if (!$DB->tableExists("glpi_queuednotifications")) {
-     $config = "CREATE TABLE `glpi_queuednotifications` (
-        `id` int(11) NOT NULL AUTO_INCREMENT,
-        `itemtype` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-        `items_id` int(11) NOT NULL DEFAULT '0',
-        `notificationtemplates_id` int(11) NOT NULL DEFAULT '0',
-        `entities_id` int(11) NOT NULL DEFAULT '0',
-        `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
-        `sent_try` int(11) NOT NULL DEFAULT '0',
-        `create_time` timestamp NULL DEFAULT NULL,
-        `send_time` timestamp NULL DEFAULT NULL,
-        `sent_time` timestamp NULL DEFAULT NULL,
-        `name` text COLLATE utf8_unicode_ci,
-        `sender` text COLLATE utf8_unicode_ci,
-        `sendername` text COLLATE utf8_unicode_ci,
-        `recipient` text COLLATE utf8_unicode_ci,
-        `recipientname` text COLLATE utf8_unicode_ci,
-        `replyto` text COLLATE utf8_unicode_ci,
-        `replytoname` text COLLATE utf8_unicode_ci,
-        `headers` text COLLATE utf8_unicode_ci,
-        `body_html` longtext COLLATE utf8_unicode_ci,
-        `body_text` longtext COLLATE utf8_unicode_ci,
-        `messageid` text COLLATE utf8_unicode_ci,
-        `documents` text COLLATE utf8_unicode_ci,
-        `mode` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT 'See Notification_NotificationTemplate::MODE_* constants',
-        PRIMARY KEY (`id`),
-        KEY `item` (`itemtype`,`items_id`,`notificationtemplates_id`),
-        KEY `is_deleted` (`is_deleted`),
-        KEY `entities_id` (`entities_id`),
-        KEY `sent_try` (`sent_try`),
-        KEY `create_time` (`create_time`),
-        KEY `send_time` (`send_time`),
-        KEY `sent_time` (`sent_time`),
-        KEY `mode` (`mode`)
-      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
-         $DB->queryOrDie($config, "erreur lors de la création de la table de configuration ".$DB->error());
-     }
+        $config = "CREATE TABLE `glpi_queuednotifications` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `itemtype` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+            `items_id` int(11) NOT NULL DEFAULT '0',
+            `notificationtemplates_id` int(11) NOT NULL DEFAULT '0',
+            `entities_id` int(11) NOT NULL DEFAULT '0',
+            `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+            `sent_try` int(11) NOT NULL DEFAULT '0',
+            `create_time` timestamp NULL DEFAULT NULL,
+            `send_time` timestamp NULL DEFAULT NULL,
+            `sent_time` timestamp NULL DEFAULT NULL,
+            `name` text COLLATE utf8_unicode_ci,
+            `sender` text COLLATE utf8_unicode_ci,
+            `sendername` text COLLATE utf8_unicode_ci,
+            `recipient` text COLLATE utf8_unicode_ci,
+            `recipientname` text COLLATE utf8_unicode_ci,
+            `replyto` text COLLATE utf8_unicode_ci,
+            `replytoname` text COLLATE utf8_unicode_ci,
+            `headers` text COLLATE utf8_unicode_ci,
+            `body_html` longtext COLLATE utf8_unicode_ci,
+            `body_text` longtext COLLATE utf8_unicode_ci,
+            `messageid` text COLLATE utf8_unicode_ci,
+            `documents` text COLLATE utf8_unicode_ci,
+            `mode` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT 'See Notification_NotificationTemplate::MODE_* constants',
+            PRIMARY KEY (`id`),
+            KEY `item` (`itemtype`,`items_id`,`notificationtemplates_id`),
+            KEY `is_deleted` (`is_deleted`),
+            KEY `entities_id` (`entities_id`),
+            KEY `sent_try` (`sent_try`),
+            KEY `create_time` (`create_time`),
+            KEY `send_time` (`send_time`),
+            KEY `sent_time` (`sent_time`),
+            KEY `mode` (`mode`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+        $DB->queryOrDie($config, "erreur lors de la création de la table de configuration ".$DB->error());
+    }
 
-      $config = "ALTER TABLE `glpi_oidc_config`
-         ADD COLUMN `scope` varchar(255) DEFAULT NULL";
-      $DB->queryOrDie($config, "erreur lors de la mise a jour de la table de configuration oidc".$DB->error());
+    // Update OIDC config table
+    $config = "ALTER TABLE `glpi_oidc_config` ADD COLUMN `scope` varchar(255) DEFAULT NULL";
+    $DB->queryOrDie($config, "erreur lors de la mise a jour de la table de configuration oidc".$DB->error());
 
-   // ************ Keep it at the end **************
-   $migration->executeMigration();
-   return $updateresult;
+    // Update users table to add accessibility columns
+    $users = "ALTER TABLE `glpi_users` ADD COLUMN `access_zoom_level` smallint(1) DEFAULT 100";
+    $DB->queryOrDie($users, "erreur lors de la mise a jour de la table de glpi_users".$DB->error());
+
+    $users = "ALTER TABLE `glpi_users` ADD COLUMN `access_font` varchar(100) DEFAULT NULL";
+    $DB->queryOrDie($users, "erreur lors de la mise a jour de la table de glpi_users".$DB->error());
+
+    $users = "ALTER TABLE `glpi_users` ADD COLUMN `access_shortcuts` tinyint(1) DEFAULT 0";
+    $DB->queryOrDie($users, "erreur lors de la mise a jour de la table de glpi_users".$DB->error());
+
+    $users = "ALTER TABLE `glpi_users` ADD COLUMN `access_custom_shortcuts` JSON DEFAULT NULL";
+    $DB->queryOrDie($users, "erreur lors de la mise a jour de la table de glpi_users".$DB->error());
+
+    // ************ Keep it at the end **************
+    $migration->executeMigration();
+    return $updateresult;
 }
