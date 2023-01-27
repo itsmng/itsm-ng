b8c838c1137e9900e40aafca52bf9b6198fbaa46
Florian Blanchet <contact@minzord.fr>
2023-01-27 15:28:37 +0100

    Add script

---
+++ .github/actions/test_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+LOG_FILE="./tests/files/_log/install.log"
+mkdir -p $(dirname "$LOG_FILE")
+
+# Execute install
+bin/console itsmng:database:install \
+  --config-dir=./tests/config --ansi --no-interaction \
+  --reconfigure --db-name=glpi --db-host=db --db-user=root --force
+
+# Execute update
+## Should do nothing.
+bin/console itsmng:database:update --config-dir=./tests/config --ansi --no-interaction | tee $LOG_FILE
+if [[ -z $(grep "No migration needed." $LOG_FILE) ]];
+  then echo "itsmng:database:update command FAILED" && exit 1;
+fi
