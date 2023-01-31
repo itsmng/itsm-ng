c3424b28af7ba5e973f1ac9fb0dee4a026d2ed82
Florian Blanchet <51517706+minzords@users.noreply.github.com>
2023-01-31 15:49:50 +0100

    Update init_build.sh

--- .github/actions/init_build.sh
+++ .github/actions/init_build.sh
@@ -3,7 +3,7 @@
 PHP_MAJOR_VERSION="$(echo $PHP_VERSION | cut -d '.' -f 1,2)"
 
 # Validate composer config
-composer validate --strict
+#composer validate --strict
 #if ! [[ "$PHP_MAJOR_VERSION" == "8.0" ]]; then
 #  composer check-platform-reqs;
 #fi
