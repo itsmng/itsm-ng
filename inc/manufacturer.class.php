41ecc91dbfd9dd3e5786f94ecf24e4f4dc890d13
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-01-12 15:16:08 +0100

    feat(manufacturer.class.php): form

--- inc/manufacturer.class.php
+++ inc/manufacturer.class.php
@@ -58,12 +58,43 @@ class Manufacturer extends CommonDropdown {
 
    function getAdditionalFields() {
 
-      return [['name'  => 'none',
-                         'label' => RegisteredID::getTypeName(Session::getPluralNumber()).
-                                       RegisteredID::showAddChildButtonForItemForm($this,
-                                                                                   '_registeredID',
-                                                                                   null, false),
-                         'type'  => 'registeredIDChooser']];
+      return [
+         RegisteredID::getTypeName(Session::getPluralNumber()) => [
+            'name'  => 'none',
+            'type'  => 'multiSelect',
+            'inputs' => [
+               [
+                  'name' => 'current_registeredID_type',
+                  'type' => 'select',
+                  'values' => array_merge([ Dropdown::EMPTY_VALUE ], RegisteredID::getRegisteredIDTypes()),
+               ],
+               [
+                  'name' => 'current_registeredID',
+                  'type' => 'text',
+                  'size' => 30,
+               ],
+            ],
+            'getInputAdd' => <<<JS
+               function () {
+                  if (!$('input[name="current_registeredID"]').val()) {
+                     return;
+                  }
+                  var values = {
+                     _registeredID_type: $('select[name="current_registeredID_type"]').val(),
+                     _registeredID: $('input[name="current_registeredID').val()
+                  };
+                  var title = $('select[name="current_registeredID_type"] option:selected').text() + ' ' + $('input[name="current_registeredID"').val();
+                  return {values, title};
+               }
+            JS,
+            'values' => getOptionsWithNameForItem('RegisteredID',
+               ['itemtype' => $this::class, 'items_id' => $this->getID()],
+               ['_registeredID_type' => 'device_type', '_registeredID' => 'name']
+            ),
+            'col_lg' => 12,
+            'col_md' => 12,
+         ],
+      ];
    }
 
 
