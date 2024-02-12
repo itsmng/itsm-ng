b349f1d1a300ca0a76d94114a14a5b3703512e06
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-02-12 11:12:43 +0100

    feat(report.contract.php, report.year.php): forms for report

--- front/report.year.php
+++ front/report.year.php
@@ -40,42 +40,46 @@ Report::title();
 
 // Titre
 
-echo "<form name='form' method='post' action='report.year.list.php'>";
-
-echo "<table class='tab_cadre_fixe'>";
-echo "<tr><th colspan='4'>".__("Equipment's report by year")."</th></tr>";
-
-// 3. Selection d'affichage pour generer la liste
-
-echo "<tr class='tab_bg_2'>";
-echo "<td width='20%' class='b center'>".__('Item type')."</td>";
-echo "<td width='30%'>";
 $values = [0 => __('All')];
 foreach ($CFG_GLPI["contract_types"] as $itemtype) {
    if ($item = getItemForItemtype($itemtype)) {
       $values[$itemtype] = $item->getTypeName();
    }
 }
-Dropdown::showFromArray('item_type', $values, ['value'    => 0,
-                                                   'multiple' => true]);
-echo "</td>";
-
-echo "<td width='20%' class='center'><p class='b'>"._n('Date', 'Dates', 1)."</p></td>";
-echo "<td width='30%'>";
-$y = date("Y");
-$values = [ 0 => __('All')];
+$y      = date("Y");
+$dates = [ 0 => __('All')];
 for ($i=($y-10); $i<($y+10); $i++) {
-   $values[$i] = $i;
+   $dates[$i] = $i;
 }
-Dropdown::showFromArray('year', $values, ['value'    => $y,
-                                              'multiple' => true]);
-echo "</td></tr>";
-
-echo "<tr class='tab_bg_2'><td colspan='4' class='center'>";
-echo "<input type='submit' value=\"".__s('Display report')."\" class='submit'></td></tr>";
-
-echo "</table>";
-
-Html::closeForm();
+$form = [
+   'action' => 'report.year.list.php',
+   'buttons' => [
+      [
+         'value' => __s('Display report'),
+         'class' => 'btn btn-secondary'
+      ]
+   ],
+   'content' => [
+      __('Hardware under contract') => [
+         'visible' => true,
+         'inputs' => [
+            __('Item type') => [
+               'type' => 'checklist',
+               'name' => 'item_type',
+               'options' => $values,
+               'col_lg' => 6,
+            ],
+            _n('Date', 'Dates', 1) => [
+               'type' => 'checklist',
+               'name' => 'year',
+               'options' => $dates,
+               'values' => [$y],
+               'col_lg' => 6,
+            ],
+         ]
+      ]
+   ]
+];
+renderTwigForm($form);
 
 Html::footer();
