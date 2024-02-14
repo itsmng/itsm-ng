3025c9ba488831777250823653633a3a4552ee59
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-02-14 10:40:15 +0100

    fix(twig.utils.php): getOptionsForItems now supports entities

--- src/twig/twig.utils.php
+++ src/twig/twig.utils.php
@@ -5,32 +5,27 @@
  */
 function getOptionForItems($item, $conditions = [], $display_emptychoice = true, $isDevice = false, $used = [])
 {
-    global $DB;
-
-    $table = getTableForItemType($item);
-$name = $isDevice ? 'designation' : 'name';
-    $iterator = $DB->request([
-        'SELECT' => ['id', $name ],
-        'FROM' => $table,
-        'WHERE' => $conditions,
-    ]);
+    $values = Dropdown::getDropdownValue([
+        'itemtype' => $item,
+        'entity_restrict' => $conditions['entities_id'] ?? Session::getActiveEntity(),
+        'used' => $used,
+        'display_emptychoice' => $display_emptychoice,
+    ], false);
 
     $options = [];
-    
-    while ($val = $iterator->next()) {
-                $options[$val['id']] = $val[$name] == '' ? '(' . $val['id'] . ')' : $val[$name];
+    foreach ($values['results'] as $key => $value) {
+        if (!$value || !count($value))
+            continue;
+
+        if (isset($value['children'])) {
+            $options[$value['text']] = [];
+            foreach ($value['children'] as $childValue) {
+                $options[$value['text']][$childValue['id']] = $childValue['text'];
             }
-        if ($display_emptychoice) {
-        if (!isset($options[0])) {
-            $options = [0 => Dropdown::EMPTY_VALUE] + $options;
-    } else {
-        $options = [-1 => Dropdown::EMPTY_VALUE] + $options;
+        } else {
+            $options[$value['id']] = $value['text'];
         }
     }
-foreach ($used as $id) {
-        unset($options[$id]);
-    }
-
     return $options;
 }
 
