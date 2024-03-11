2c14f46e90fb7f6a6f17a59fdd4af6bc57682c27
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-03-11 14:32:13 +0100

    feat(user.class.php): profile image picker

--- inc/itsmnguploadhandler.class.php
+++ inc/itsmnguploadhandler.class.php
@@ -125,4 +125,10 @@ class ItsmngUploadHandler {
          'users_id'     => $userId,
       ]);
    }
+
+   static function removeTmpFiles($files) {
+      foreach ($files as $file) {
+         unlink($file['path']);
+      }
+   }
 }
