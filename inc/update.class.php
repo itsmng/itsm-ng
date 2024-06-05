13e06ba708654ea00c6ba60ede605f6dff2a1c14
minzords <florian.blanchet@itsm-ng.com>
2024-06-05 14:12:28 +0200

    🔖 1.6.4

--- inc/update.class.php
+++ inc/update.class.php
@@ -532,6 +532,7 @@ class Update extends CommonGLPI {
          case "1.6.0":
          case "1.6.1":
          case "1.6.2":
+         case "1.6.3":
             include_once "{$updir}itsm_update_160_161.php";
             update160to161();
             break;
