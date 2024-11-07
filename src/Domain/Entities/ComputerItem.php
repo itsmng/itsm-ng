2da50850dd7b0f57af0c523fe8162c8ac21ecb4f
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-07 16:55:34 +0100

    feat: ComputerItem entity

---
+++ src/Domain/Entities/ComputerItem.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_computers_items')]
+#[ORM\Index(name: 'computers_id', columns: ['computers_id'])]
+#[ORM\Index(name: 'itemtype_items_id', columns: ['itemtype', 'items_id'])]
+#[ORM\Index(name: 'is_deleted', columns: ['is_deleted'])]
+#[ORM\Index(name: 'is_dynamic', columns: ['is_dynamic'])]
+
+class ComputerItem
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0, 'comment' => 'RELATION to various table, according to itemtype (ID)'])]
+    private $items_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $computers_id;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $itemtype;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_deleted;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_dynamic;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getComputersId(): ?int
+    {
+        return $this->computers_id;
+    }
+
+    public function setComputersId(int $computers_id): self
+    {
+        $this->computers_id = $computers_id;
+
+        return $this;
+    }
+
+    public function getItemsId(): ?int
+    {
+        return $this->items_id;
+    }
+
+    public function setItemsId(int $items_id): self
+    {
+        $this->items_id = $items_id;
+
+        return $this;
+    }
+
+    public function getItemtype(): ?string
+    {
+        return $this->itemtype;
+    }
+
+    public function setItemtype(?string $itemtype): self
+    {
+        $this->itemtype = $itemtype;
+
+        return $this;
+    }
+
+    public function getIsDeleted(): ?int
+    {
+        return $this->is_deleted;
+    }
+
+    public function setIsDeleted(int $is_deleted): self
+    {
+        $this->is_deleted = $is_deleted;
+
+        return $this;
+    }
+
+    public function getIsDynamic(): ?int
+    {
+        return $this->is_dynamic;
+    }
+
+    public function setIsDynamic(int $is_dynamic): self
+    {
+        $this->is_dynamic = $is_dynamic;
+
+        return $this;
+    }
+}
