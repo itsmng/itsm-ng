0802e177a3e712e9b0abf295420388ecd79f7bc6
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-07 16:18:40 +0100

    feat: ChangeTemplate entity

---
+++ src/Domain/Entities/ChangeTemplate.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "glpi_changetemplates")]
+#[ORM\Index(name: "name", columns: ["name"])]
+#[ORM\Index(name: "entities_id", columns: ["entities_id"])]
+#[ORM\Index(name: "is_recursive", columns: ["is_recursive"])]
+class ChangeTemplate
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private $id;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: "integer", options: ['default' => 0])]
+    private $entities_id;
+
+    #[ORM\Column(type: "boolean", options: ['default' => false])]
+    private $is_recursive;
+
+    #[ORM\Column(type: "text", nullable: true, length: 65535)]
+    private $comment;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEntitiesId(): ?int
+    {
+        return $this->entities_id;
+    }
+
+    public function setEntitiesId(?int $entities_id): self
+    {
+        $this->entities_id = $entities_id;
+
+        return $this;
+    }
+
+    public function getIsRecursive(): ?bool
+    {
+        return $this->is_recursive;
+    }
+
+    public function setIsRecursive(?bool $is_recursive): self
+    {
+        $this->is_recursive = $is_recursive;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+}
