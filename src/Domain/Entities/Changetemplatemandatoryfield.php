483bcf4305938ae7e7f29055678421afecf6ff97
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-07 16:12:01 +0100

    feat:Changetemplatemandatoryfield entity

---
+++ src/Domain/Entities/Changetemplatemandatoryfield.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_changetemplatemandatoryfields')]
+#[ORM\UniqueConstraint(name: 'changetemplates_id_num', columns: ['changetemplates_id', 'num'])]
+#[ORM\Index(name: 'changetemplates_id', columns: ['changetemplates_id'])]
+
+class Changetemplatemandatoryfield
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $changetemplates_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $num;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getChangetemplatesId(): ?int
+    {
+        return $this->changetemplates_id;
+    }
+
+    public function setChangetemplatesId(int $changetemplates_id): self
+    {
+        $this->changetemplates_id = $changetemplates_id;
+
+        return $this;
+    }
+
+    public function getNum(): ?int
+    {
+        return $this->num;
+    }
+
+    public function setNum(int $num): self
+    {
+        $this->num = $num;
+
+        return $this;
+    }
+}   
