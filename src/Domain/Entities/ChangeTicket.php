a40c91b5dc42b4a76166128f4e57545c68e3602e
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-07 15:58:35 +0100

    feat: ChangeTicket entity

---
+++ src/Domain/Entities/ChangeTicket.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_changes_tickets')]
+#[ORM\UniqueConstraint(columns: ['changes_id', 'tickets_id'])]
+#[ORM\Index(columns: ['tickets_id'])]
+class ChangeTicket
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $changes_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $tickets_id;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getChangesId(): int
+    {
+        return $this->changes_id;
+    }
+
+    public function setChangesId(int $changes_id): self
+    {
+        $this->changes_id = $changes_id;
+
+        return $this;
+    }
+
+    public function getTicketsId(): int
+    {
+        return $this->tickets_id;
+    }
+
+    public function setTicketsId(int $problems_id): self
+    {
+        $this->tickets_id = $problems_id;
+
+        return $this;
+    }
+}
