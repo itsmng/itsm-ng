3f41d31293d80226aaa82c6aabd7119407b5be29
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-07 16:04:32 +0100

    feat: ChangeUser Entity

---
+++ src/Domain/Entities/ChangeUser.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_changes_users')]
+#[ORM\UniqueConstraint(name: 'changes_id_type_users_id_alternative_email', columns: ['changes_id', 'type', 'users_id', 'alternative_email'])]
+#[ORM\Index(name: 'users_id_type', columns: ['users_id', 'type'])]
+
+class ChangeUser
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $changes_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $users_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 1])]
+    private $type;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => false])]
+    private $use_notification;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $alternative_email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getChangesId(): ?int
+    {
+        return $this->changes_id;
+    }
+
+    public function setChangesId(int $changes_id): self
+    {
+        $this->changes_id = $changes_id;
+
+        return $this;
+    }
+
+    public function getUsersId(): ?int
+    {
+        return $this->users_id;
+    }    
+
+    public function setUsersId(int $users_id): self
+    {
+        $this->users_id = $users_id;
+
+        return $this;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(int $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getUseNotification(): ?bool
+    {
+        return $this->use_notification;
+    }
+
+    public function setUseNotification(bool $use_notification): self
+    {
+        $this->use_notification = $use_notification;
+
+        return $this;
+    }
+
+    public function getAlternativeEmail(): ?string
+    {
+        return $this->alternative_email;
+    }
+
+    public function setAlternativeEmail(?string $alternative_email): self
+    {
+        $this->alternative_email = $alternative_email;
+
+        return $this;
+    }
+}
