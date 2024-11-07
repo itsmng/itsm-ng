eb677b8a39b29529d5546ff89636e0b105cd6273
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-07 11:21:09 +0100

    feat: first table

---
+++ src/Domain/Entities/Alert.php
@@ -0,0 +1,89 @@
+<?php
+//Column	Type	Comment
+//id	int(11) Auto Increment
+//itemtype	varchar(100)
+//items_id	int(11) [0]
+//type	int(11) [0]	see define.php ALERT_* constant
+//date	timestamp [current_timestamp()]
+
+namespace itsm\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_alerts')]
+#[ORM\UniqueConstraint(name: 'alert_itemtype_items_id_type', columns: ['itemtype', 'items_id', 'type'])]
+#[ORM\Index(name: 'type', columns: ['type'])]
+#[ORM\Index(name: 'date', columns: ['date'])]
+class Alert
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $itemtype;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $items_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0, 'comment' => 'see define.php ALERT_* constant'])]
+    private $type;
+
+    #[ORM\Column(type: 'datetime')]
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getItemtype(): ?string
+    {
+        return $this->itemtype;
+    }
+
+    public function setItemtype(string $itemtype): self
+    {
+        $this->itemtype = $itemtype;
+
+        return $this;
+    }
+
+    public function getItemsId(): ?int
+    {
+        return $this->items_id;
+    }
+
+    public function setItemsId(int $items_id): self
+    {
+        $this->items_id = $items_id;
+
+        return $this;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(int $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
