28276008bed81e04e2d1544e49f8f0a4a643e064
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-07 15:55:34 +0100

    feat: ChangeProblem entity

---
+++ src/Domain/Entities/ChangeProblem.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/*
+Column	Type	Comment
+id	int(11) Auto Increment
+changes_id	int(11) [0]
+problems_id	int(11) [0]
+Indexes
+PRIMARY	id
+UNIQUE	changes_id, problems_id
+INDEX	problems_id
+ */
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_changes_problems')]
+#[ORM\UniqueConstraint(columns: ['changes_id', 'problems_id'])]
+#[ORM\Index(name: 'problems_id', columns: ['problems_id'])]
+class ChangeProblem
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $changes_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $problems_id;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getChangesId(): int
+    {
+        return $this->changes_id;
+    }
+
+    public function setChangesId(int $changes_id): self
+    {
+        $this->changes_id = $changes_id;
+
+        return $this;
+    }
+
+    public function getProblemsId(): int
+    {
+        return $this->problems_id;
+    }
+
+    public function setProblemsId(int $problems_id): self
+    {
+        $this->problems_id = $problems_id;
+
+        return $this;
+    }
+}
