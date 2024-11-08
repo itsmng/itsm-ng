a27e1c3b5d00a35514656236e8f2b10deff1f98e
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-08 10:14:47 +0100

    feat: Consumable Entity

---
+++ src/Domain/Entities/Consumable.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_consumables')]
+#[ORM\Index(name: 'date_in', columns: ['date_in'])]
+#[ORM\Index(name: 'date_out', columns: ['date_out'])]
+#[ORM\Index(name: 'consumableitems_id', columns: ['consumableitems_id'])]
+#[ORM\Index(name: 'entities_id', columns: ['entities_id'])]
+#[ORM\Index(name: 'itemtype_items_id', columns: ['itemtype', 'items_id'])]
+#[ORM\Index(name: 'date_mod', columns: ['date_mod'])]
+#[ORM\Index(name: 'date_creation', columns: ['date_creation'])]
+
+class Consumable
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $entities_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $consumableitems_id;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private $date_in;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private $date_out;
+
+    #[ORM\Column(type: 'string', length: 100, nullable: true)]
+    private $itemtype;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $items_id;
+
+    #[ORM\Column(type: 'datetime', options: ['default' => 'CURRENT_TIMESTAMP'], nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: 'datetime', options: ['default' => 'CURRENT_TIMESTAMP'], nullable: true)]
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEntitiesId(): ?int
+    {
+        return $this->entities_id;
+    }
+
+    public function setEntitiesId(int $entities_id): self
+    {
+        $this->entities_id = $entities_id;
+
+        return $this;
+    }
+
+    public function getConsumableitemsId(): ?int
+    {
+        return $this->consumableitems_id;
+    }
+
+    public function setConsumableitemsId(int $consumableitems_id): self
+    {
+        $this->consumableitems_id = $consumableitems_id;
+
+        return $this;
+    }
+
+    public function getDateIn(): ?\DateTimeInterface
+    {
+        return $this->date_in;
+    }
+
+    public function setDateIn(\DateTimeInterface $date_in): self
+    {
+        $this->date_in = $date_in;
+
+        return $this;
+    }
+
+    public function getDateOut(): ?\DateTimeInterface
+    {
+        return $this->date_out;
+    }
+
+    public function setDateOut(\DateTimeInterface $date_out): self
+    {
+        $this->date_out = $date_out;
+
+        return $this;
+    }
+
+    public function getItemtype(): ?string
+    {
+        return $this->itemtype;
+    }
+
+    public function setItemtype(?string $itemtype): self
+    {
+        $this->itemtype = $itemtype;
+
+        return $this;
+    }
+
+    public function getItemsId(): ?int
+    {
+        return $this->items_id;
+    }
+
+    public function setItemsId(int $items_id): self
+    {
+        $this->items_id = $items_id;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+}
