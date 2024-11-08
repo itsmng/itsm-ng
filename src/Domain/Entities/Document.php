ea78a1e3034d0ad75b3b7a3d96acde827ffa6661
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-08 16:26:32 +0100

    feat: add Document entity

---
+++ src/Domain/Entities/Document.php
@@ -0,0 +1,292 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_documents')]
+#[ORM\Index(name: 'date_mod', columns: ['date_mod'])]
+#[ORM\Index(name: 'name', columns: ['name'])]
+#[ORM\Index(name: 'entities_id', columns: ['entities_id'])]
+#[ORM\Index(name: 'tickets_id', columns: ['tickets_id'])]
+#[ORM\Index(name: 'users_id', columns: ['users_id'])]
+#[ORM\Index(name: 'documentcategories_id', columns: ['documentcategories_id'])]
+#[ORM\Index(name: 'is_deleted', columns: ['is_deleted'])]
+#[ORM\Index(name: 'sha1sum', columns: ['sha1sum'])]
+#[ORM\Index(name: 'tag', columns: ['tag'])]
+#[ORM\Index(name: 'date_creation', columns: ['date_creation'])]
+class Document
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $entities_id;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => false])]
+    private $is_recursive;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true, options: ['comment' => 'for display and transfert'])]
+    private $filename;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true, options: ['comment' => 'file storage path'])]
+    private $filepath;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $documentcategories_id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $mime;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: 'text', nullable: true, length: 65535)]
+    private $comment;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => false])]
+    private $is_deleted;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $link;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $users_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $tickets_id;
+
+    #[ORM\Column(type: 'string', length: 40, nullable: true, options: ['fixed' => true])]
+    private $sha1sum;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => false])]
+    private $is_blacklisted;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $tag;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getEntitiesId(): ?int
+    {
+        return $this->entities_id;
+    }
+
+    public function setEntitiesId(int $entities_id): self
+    {
+        $this->entities_id = $entities_id;
+
+        return $this;
+    }
+
+    public function getIsRecursive(): ?bool
+    {
+        return $this->is_recursive;
+    }
+
+    public function setIsRecursive(bool $is_recursive): self
+    {
+        $this->is_recursive = $is_recursive;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): self
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    public function getFilepath(): ?string
+    {
+        return $this->filepath;
+    }
+
+    public function setFilepath(string $filepath): self
+    {
+        $this->filepath = $filepath;
+
+        return $this;
+    }
+
+    public function getDocumentcategoriesId(): ?int
+    {
+        return $this->documentcategories_id;
+    }
+
+    public function setDocumentcategoriesId(int $documentcategories_id): self
+    {
+        $this->documentcategories_id = $documentcategories_id;
+
+        return $this;
+    }
+
+    public function getMime(): ?string
+    {
+        return $this->mime;
+    }
+
+    public function setMime(string $mime): self
+    {
+        $this->mime = $mime;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getIsDeleted(): ?bool
+    {
+        return $this->is_deleted;
+    }
+
+    public function setIsDeleted(bool $is_deleted): self
+    {
+        $this->is_deleted = $is_deleted;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getUsersId(): ?int
+    {
+        return $this->users_id;
+    }
+
+    public function setUsersId(int $users_id): self
+    {
+        $this->users_id = $users_id;
+
+        return $this;
+    }
+
+    public function getTicketsId(): ?int
+    {
+        return $this->tickets_id;
+    }
+
+    public function setTicketsId(int $tickets_id): self
+    {
+        $this->tickets_id = $tickets_id;
+
+        return $this;
+    }
+
+    public function getSha1sum(): ?string
+    {
+        return $this->sha1sum;
+    }
+
+    public function setSha1sum(string $sha1sum): self
+    {
+        $this->sha1sum = $sha1sum;
+
+        return $this;
+    }
+
+    public function getIsBlacklisted(): ?bool
+    {
+        return $this->is_blacklisted;
+    }
+
+    public function setIsBlacklisted(bool $is_blacklisted): self
+    {
+        $this->is_blacklisted = $is_blacklisted;
+
+        return $this;
+    }
+
+    public function getTag(): ?string
+    {
+        return $this->tag;
+    }
+
+    public function setTag(string $tag): self
+    {
+        $this->tag = $tag;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+}
