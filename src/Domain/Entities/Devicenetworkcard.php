fc2e403694d699def05be7938852b320dab8e7d5
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-08 15:47:28 +0100

    feat: Devicenetworkcard Entity

---
+++ src/Domain/Entities/Devicenetworkcard.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_devicenetworkcards')]  
+#[ORM\Index(name: 'designation', columns: ['designation'])]            
+#[ORM\Index(name: 'manufacturers_id', columns: ['manufacturers_id'])]
+#[ORM\Index(name: 'entities_id', columns: ['entities_id'])]
+#[ORM\Index(name: 'is_recursive', columns: ['is_recursive'])]
+#[ORM\Index(name: 'date_mod', columns: ['date_mod'])]
+#[ORM\Index(name: 'date_creation', columns: ['date_creation'])]
+#[ORM\Index(name: 'devicenetworkcardmodels_id', columns: ['devicenetworkcardmodels_id'])]
+class Devicenetworkcard
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $designation;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $bandwidth;
+
+    #[ORM\Column(type: 'text', length: 65535, nullable: true)]
+    private $comment;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $manufacturers_id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $mac_default;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $entities_id;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_recursive;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $devicenetworkcardmodels_id;
+
+    #[ORM\Column(type: 'datetime', options: ['default' => 'CURRENT_TIMESTAMP'], nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: 'datetime', options: ['default' => 'CURRENT_TIMESTAMP'], nullable: true)]
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDesignation(): ?string
+    {
+        return $this->designation;
+    }
+
+    public function setDesignation(?string $designation): self
+    {
+        $this->designation = $designation;
+
+        return $this;
+    }
+
+    public function getBandwidth(): ?string
+    {
+        return $this->bandwidth;
+    }
+
+    public function setBandwidth(?string $bandwidth): self
+    {
+        $this->bandwidth = $bandwidth;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getManufacturersId(): ?int
+    {
+        return $this->manufacturers_id;
+    }
+
+    public function setManufacturersId(?int $manufacturers_id): self
+    {
+        $this->manufacturers_id = $manufacturers_id;
+
+        return $this;
+    }
+
+    public function getMacDefault(): ?string
+    {
+        return $this->mac_default;
+    }
+
+    public function setMacDefault(?string $mac_default): self
+    {
+        $this->mac_default = $mac_default;
+
+        return $this;
+    }
+
+    public function getEntitiesId(): ?int
+    {
+        return $this->entities_id;
+    }
+
+    public function setEntitiesId(?int $entities_id): self
+    {
+        $this->entities_id = $entities_id;
+
+        return $this;
+    }
+
+    public function getIsRecursive(): ?bool
+    {
+        return $this->is_recursive;
+    }
+
+    public function setIsRecursive(?bool $is_recursive): self
+    {
+        $this->is_recursive = $is_recursive;
+
+        return $this;
+    }
+
+    public function getDevicenetworkcardmodelsId(): ?int
+    {
+        return $this->devicenetworkcardmodels_id;
+    }        
+
+    public function setDevicenetworkcardmodelsId(?int $devicenetworkcardmodels_id): self
+    {
+        $this->devicenetworkcardmodels_id = $devicenetworkcardmodels_id;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(?\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }  
+
+    public function setDateCreation(?\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+    
+}
+
+
+
+
