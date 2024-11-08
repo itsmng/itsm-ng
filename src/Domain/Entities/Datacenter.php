c15ee5c01f948ad7c95024b0c20a864c0878ab26
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-08 14:12:12 +0100

    feat: Datacenter Entity

---
+++ src/Domain/Entities/Datacenter.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_datacenters')]
+#[ORM\Index(name: 'entities_id', columns: ['entities_id'])]
+#[ORM\Index(name: 'is_recursive', columns: ['is_recursive'])]
+#[ORM\Index(name: 'locations_id', columns: ['locations_id'])]
+#[ORM\Index(name: 'is_deleted', columns: ['is_deleted'])]
+
+class Datacenter
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $entities_id;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_recursive;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $locations_id;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_deleted;
+
+    #[ORM\Column(type: 'datetime', options: ['default' => 'CURRENT_TIMESTAMP'], nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: 'datetime', options: ['default' => 'CURRENT_TIMESTAMP'], nullable: true)]
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEntitiesId(): ?int
+    {
+        return $this->entities_id;
+    }
+
+    public function setEntitiesId(int $entities_id): self
+    {
+        $this->entities_id = $entities_id;
+
+        return $this;
+    }
+
+    public function getIsRecursive(): ?int
+    {
+        return $this->is_recursive;
+    }
+
+    public function setIsRecursive(int $is_recursive): self
+    {
+        $this->is_recursive = $is_recursive;
+
+        return $this;
+    }
+
+    public function getLocationsId(): ?int
+    {
+        return $this->locations_id;
+    }
+
+    public function setLocationsId(int $locations_id): self
+    {
+        $this->locations_id = $locations_id;
+
+        return $this;
+    }
+
+    public function getIsDeleted(): ?int
+    {
+        return $this->is_deleted;
+    }
+
+    public function setIsDeleted(int $is_deleted): self
+    {
+        $this->is_deleted = $is_deleted;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+}   
