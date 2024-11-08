a7c2bb19aac2a451feb667e7bba2f1209ac30c49
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-08 16:59:13 +0100

    feat: EntityKnowbaseitem Entity

---
+++ src/Domain/Entities/EntityKnowbaseitem.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_entities_knowbaseitems')]  
+#[ORM\Index(name: 'knowbaseitems_id', columns: ['knowbaseitems_id'])]
+#[ORM\Index(name: 'entities_id', columns: ['entities_id'])]
+#[ORM\Index(name: 'is_recursive', columns: ['is_recursive'])]
+class EntityKnowbaseitem
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $knowbaseitems_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $entities_id;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_recursive;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getKnowbaseitemsId(): ?int
+    {
+        return $this->knowbaseitems_id;
+    }
+
+    public function setKnowbaseitemsId(?int $knowbaseitems_id): self
+    {
+        $this->knowbaseitems_id = $knowbaseitems_id;
+
+        return $this;
+    }
+
+    public function getEntitiesId(): ?int
+    {
+        return $this->entities_id;
+    }
+
+    public function setEntitiesId(?int $entities_id): self
+    {
+        $this->entities_id = $entities_id;
+
+        return $this;
+    }
+
+    public function getIsRecursive(): ?bool
+    {
+        return $this->is_recursive;
+    }
+
+    public function setIsRecursive(?bool $is_recursive): self
+    {
+        $this->is_recursive = $is_recursive;
+
+        return $this;
+    }
+
+}       
