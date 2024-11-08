62bf49ea54fbf3ea9165b4923106c7f4f7ace68b
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-08 16:57:20 +0100

    feat: add Enclosure entity

---
+++ src/Domain/Entities/Enclosure.php
@@ -0,0 +1,314 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_enclosures')]
+#[ORM\Index(name: 'entities_id', columns: ['entities_id'])]
+#[ORM\Index(name: 'is_recursive', columns: ['is_recursive'])]
+#[ORM\Index(name: 'locations_id', columns: ['locations_id'])]
+#[ORM\Index(name: 'enclosuremodels_id', columns: ['enclosuremodels_id'])]
+#[ORM\Index(name: 'users_id_tech', columns: ['users_id_tech'])]
+#[ORM\Index(name: 'groups_id_tech', columns: ['groups_id_tech'])]
+#[ORM\Index(name: 'is_template', columns: ['is_template'])]
+#[ORM\Index(name: 'is_deleted', columns: ['is_deleted'])]
+#[ORM\Index(name: 'states_id', columns: ['states_id'])]
+#[ORM\Index(name: 'manufacturers_id', columns: ['manufacturers_id'])]
+class Enclosure {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $entities_id;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_recursive;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $locations_id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $serial;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $otherserial;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $enclosuremodels_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $users_id_tech;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $groups_id_tech;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_template;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $template_name;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_deleted;
+
+    #[ORM\Column(type: 'boolean', nullable: true)]
+    private $orientation;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $power_supplies;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0, 'comment' => 'RELATION to states (id)'])]
+    private $states_id;
+
+    #[ORM\Column(type: 'text', nullable: true, length: 65535)]
+    private $comment;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $manufacturers_id;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEntitiesId(): ?int
+    {
+        return $this->entities_id;
+    }
+
+    public function setEntitiesId(int $entities_id): self
+    {
+        $this->entities_id = $entities_id;
+
+        return $this;
+    }
+
+    public function getIsRecursive(): ?bool
+    {
+        return $this->is_recursive;
+    }
+
+    public function setIsRecursive(bool $is_recursive): self
+    {
+        $this->is_recursive = $is_recursive;
+
+        return $this;
+    }
+
+    public function getLocationsId(): ?int
+    {
+        return $this->locations_id;
+    }
+
+    public function setLocationsId(int $locations_id): self
+    {
+        $this->locations_id = $locations_id;
+
+        return $this;
+    }
+
+    public function getSerial(): ?string
+    {
+        return $this->serial;
+    }
+
+    public function setSerial(string $serial): self
+    {
+        $this->serial = $serial;
+
+        return $this;
+    }
+
+    public function getOtherserial(): ?string
+    {
+        return $this->otherserial;
+    }
+
+    public function setOtherserial(string $otherserial): self
+    {
+        $this->otherserial = $otherserial;
+
+        return $this;
+    }
+
+    public function getEnclosuremodelsId(): ?int
+    {
+        return $this->enclosuremodels_id;
+    }
+
+    public function setEnclosuremodelsId(int $enclosuremodels_id): self
+    {
+        $this->enclosuremodels_id = $enclosuremodels_id;
+
+        return $this;
+    }
+
+    public function getUsersIdTech(): ?int
+    {
+        return $this->users_id_tech;
+    }
+
+    public function setUsersIdTech(int $users_id_tech): self
+    {
+        $this->users_id_tech = $users_id_tech;
+
+        return $this;
+    }
+
+    public function getGroupsIdTech(): ?int
+    {
+        return $this->groups_id_tech;
+    }
+
+    public function setGroupsIdTech(int $groups_id_tech): self
+    {
+        $this->groups_id_tech = $groups_id_tech;
+
+        return $this;
+    }
+
+    public function getIsTemplate(): ?bool
+    {
+        return $this->is_template;
+    }
+
+    public function setIsTemplate(bool $is_template): self
+    {
+        $this->is_template = $is_template;
+
+        return $this;
+    }
+
+    public function getTemplateName(): ?string
+    {
+        return $this->template_name;
+    }
+
+    public function setTemplateName(string $template_name): self
+    {
+        $this->template_name = $template_name;
+
+        return $this;
+    }
+
+    public function getIsDeleted(): ?bool
+    {
+        return $this->is_deleted;
+    }
+
+    public function setIsDeleted(bool $is_deleted): self
+    {
+        $this->is_deleted = $is_deleted;
+
+        return $this;
+    }
+
+    public function getOrientation(): ?int
+    {
+        return $this->orientation;
+    }
+
+    public function setOrientation(int $orientation): self
+    {
+        $this->orientation = $orientation;
+
+        return $this;
+    }
+
+    public function getPowerSupplies(): ?bool
+    {
+        return $this->power_supplies;
+    }
+
+    public function setPowerSupplies(bool $power_supplies): self
+    {
+        $this->power_supplies = $power_supplies;
+
+        return $this;
+    }
+
+    public function getStatesId(): ?State
+    {
+        return $this->states_id;
+    }
+
+    public function setStatesId(?State $states_id): self
+    {
+        $this->states_id = $states_id;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getManufacturersId(): ?int
+    {
+        return $this->manufacturers_id;
+    }
+
+    public function setManufacturersId(?int $manufacturers_id): self
+    {
+        $this->manufacturers_id = $manufacturers_id;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+}
