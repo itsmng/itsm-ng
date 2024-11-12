930f510c7ab65150b3f9df46bbace7566b65e2e1
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-12 16:35:01 +0100

    feat: add Holiday entity

---
+++ src/Domain/Entities/Holiday.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "glpi_holidays")]
+#[ORM\Index(columns: ["name"])]
+#[ORM\Index(columns: ["begin_date"])]
+#[ORM\Index(columns: ["end_date"])]
+#[ORM\Index(columns: ["is_perpetual"])]
+#[ORM\Index(columns: ["date_mod"])]
+#[ORM\Index(columns: ["date_creation"])]
+class Holiday
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private $id;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    private $entities_id;
+
+    #[ORM\Column(type: "boolean", options: ["default" => false])]
+    private $is_recursive;
+
+    #[ORM\Column(type: "text", nullable: true, length: 65535)]
+    private $comment;
+
+    #[ORM\Column(type: "date", nullable: true)]
+    private $begin_date;
+
+    #[ORM\Column(type: "date", nullable: true)]
+    private $end_date;
+
+    #[ORM\Column(type: "boolean", options: ["default" => false])]
+    private $is_perpetual;
+
+    #[ORM\Column(type: "datetime", nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: "datetime", nullable: true)]
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEntitiesId(): ?int
+    {
+        return $this->entities_id;
+    }
+
+    public function setEntitiesId(?int $entities_id): self
+    {
+        $this->entities_id = $entities_id;
+
+        return $this;
+    }
+
+    public function getIsRecursive(): ?bool
+    {
+        return $this->is_recursive;
+    }
+
+    public function setIsRecursive(?bool $is_recursive): self
+    {
+        $this->is_recursive = $is_recursive;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getBeginDate(): ?\DateTimeInterface
+    {
+        return $this->begin_date;
+    }
+
+    public function setBeginDate(?\DateTimeInterface $begin_date): self
+    {
+        $this->begin_date = $begin_date;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->end_date;
+    }
+
+    public function setEndDate(?\DateTimeInterface $end_date): self
+    {
+        $this->end_date = $end_date;
+
+        return $this;
+    }
+
+    public function getIsPerpetual(): ?bool
+    {
+        return $this->is_perpetual;
+    }
+
+    public function setIsPerpetual(?bool $is_perpetual): self
+    {
+        $this->is_perpetual = $is_perpetual;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(?\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(?\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+}
