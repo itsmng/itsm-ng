3f6e245dbe36055ce46c5fa5fccc6b3f22497097
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-12 10:46:50 +0100

    feat: add Event entity

---
+++ src/Domain/Entities/Event.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "glpi_events")]
+#[Orm\Index(columns: ["date"])]
+#[Orm\Index(columns: ["level"])]
+#[Orm\Index(columns: ["type", "items_id"])]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private $id;
+
+    #[ORM\Column(type: "integer", options: ['default' => 0])]
+    private $items_id;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private $type;
+
+    #[ORM\Column(type: "datetime", nullable: true)]
+    private $date;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private $service;
+
+    #[ORM\Column(type: "integer", options: ['default' => 0])]
+    private $level;
+
+    #[ORM\Column(type: "text", length: 65535, nullable: true)]
+    private $message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getItemsId(): ?int
+    {
+        return $this->items_id;
+    }
+
+    public function setItemsId(int $items_id): self
+    {
+        $this->items_id = $items_id;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getService(): ?string
+    {
+        return $this->service;
+    }
+
+    public function setService(string $service): self
+    {
+        $this->service = $service;
+
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
