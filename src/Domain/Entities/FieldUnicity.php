19856e068ae695566d081a032cf62b47c6c13414
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-12 11:11:59 +0100

    feat: add FieldUnicity entity

---
+++ src/Domain/Entities/FieldUnicity.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_fieldunicities')]
+#[ORM\Index(columns: ['date_mod'])]
+#[ORM\Index(columns: ['date_creation'])]
+class FieldUnicity
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, options: ['default' => ''])]
+    private $name;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_recursive;
+
+    #[ORM\Column(type: 'string', length: 255, options: ['default' => ''])]
+    private $itemtype;
+
+    #[ORM\Column(type: 'integer', options: ['default' => -1])]
+    private $entities_id;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $fields;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_active;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $action_refuse;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $action_notify;
+
+    #[ORM\Column(type: 'text', nullable: true, length: 65535)]
+    private $comment;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getIsRecursive(): ?bool
+    {
+        return $this->is_recursive;
+    }
+
+    public function setIsRecursive(bool $is_recursive): self
+    {
+        $this->is_recursive = $is_recursive;
+
+        return $this;
+    }
+
+    public function getItemtype(): ?string
+    {
+        return $this->itemtype;
+    }
+
+    public function setItemtype(string $itemtype): self
+    {
+        $this->itemtype = $itemtype;
+
+        return $this;
+    }
+
+    public function getEntitiesId(): ?int
+    {
+        return $this->entities_id;
+    }
+
+    public function setEntitiesId(int $entities_id): self
+    {
+        $this->entities_id = $entities_id;
+
+        return $this;
+    }
+
+    public function getFields(): ?string
+    {
+        return $this->fields;
+    }
+
+    public function setFields(string $fields): self
+    {
+        $this->fields = $fields;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->is_active;
+    }
+
+    public function setIsActive(bool $is_active): self
+    {
+        $this->is_active = $is_active;
+
+        return $this;
+    }
+
+    public function getActionRefuse(): ?bool
+    {
+        return $this->action_refuse;
+    }
+
+    public function setActionRefuse(bool $action_refuse): self
+    {
+        $this->action_refuse = $action_refuse;
+
+        return $this;
+    }
+
+    public function getActionNotify(): ?bool
+    {
+        return $this->action_notify;
+    }
+
+    public function setActionNotify(bool $action_notify): self
+    {
+        $this->action_notify = $action_notify;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+}
