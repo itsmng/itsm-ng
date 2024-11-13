6f547591ae0e6698751ed40dc40ac35046870add
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-13 15:41:54 +0100

    feat: add KnowbaseitemComment entity

---
+++ src/Domain/Entities/KnowbaseitemComment.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+//Table => glpi_knowbaseitems_comments
+//
+//Select data Show structure Alter table New item
+//Column	Type	Comment
+//id	int(11) Auto Increment
+//knowbaseitems_id	int(11)
+//users_id	int(11) [0]
+//language	varchar(10) NULL
+//comment	text
+//parent_comment_id	int(11) NULL
+//date_creation	timestamp NULL
+//date_mod	timestamp NULL
+
+#[ORM\Entity]
+#[ORM\Table(name: "glpi_knowbaseitems_comments")]
+class KnowbaseitemComment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private $id;
+
+    #[ORM\Column(type: "integer")]
+    private $knowbaseitems_id;
+
+    #[ORM\Column(type: "integer", options: ['default' => 0])]
+    private $users_id;
+
+    #[ORM\Column(type: "string", length: 10, nullable: true)]
+    private $language;
+
+    #[ORM\Column(type: "text", length: 65535)]
+    private $comment;
+
+    #[ORM\Column(type: "integer", nullable: true)]
+    private $parent_comment_id;
+
+    #[ORM\Column(type: "datetime", nullable: true)]
+    private $date_creation;
+
+    #[ORM\Column(type: "datetime", nullable: true)]
+    private $date_mod;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getKnowbaseitemsId(): ?int
+    {
+        return $this->knowbaseitems_id;
+    }
+
+    public function setKnowbaseitemsId(int $knowbaseitems_id): self
+    {
+        $this->knowbaseitems_id = $knowbaseitems_id;
+
+        return $this;
+    }
+
+    public function getUsersId(): ?int
+    {
+        return $this->users_id;
+    }
+
+    public function setUsersId(int $users_id): self
+    {
+        $this->users_id = $users_id;
+
+        return $this;
+    }
+
+    public function getLanguage(): ?string
+    {
+        return $this->language;
+    }
+
+    public function setLanguage(string $language): self
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getParentCommentId(): ?int
+    {
+        return $this->parent_comment_id;
+    }
+
+    public function setParentCommentId(?int $parent_comment_id): self
+    {
+        $this->parent_comment_id = $parent_comment_id;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+}
