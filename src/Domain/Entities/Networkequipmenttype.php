44cc52650d199247f6ceeba91d0fb4aafcd0a094
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-13 16:56:31 +0100

    feat: add Networkequipmentmodel entity

---
+++ src/Domain/Entities/Networkequipmenttype.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_networkequipmenttypes')]
+#[ORM\Index(columns: ['name'])]
+#[ORM\Index(columns: ['date_mod'])]
+#[ORM\Index(columns: ['date_creation'])]
+class Networkequipmenttype
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: 'text', nullable: true, length: 65535)]
+    private $comment;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+}
