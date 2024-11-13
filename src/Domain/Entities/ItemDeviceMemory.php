b528bc530c46f6b0d03477ecdbaf774342fa2afc
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-13 11:07:40 +0100

    feat: add itemdevicememory entity

---
+++ src/Domain/Entities/ItemDeviceMemory.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "item_device_memory")]
+#[ORM\Index(columns: ["items_id"])]
+#[ORM\Index(columns: ["devicememories_id"])]
+#[ORM\Index(columns: ["size"])]
+#[ORM\Index(columns: ["is_deleted"])]
+#[ORM\Index(columns: ["is_dynamic"])]
+#[ORM\Index(columns: ["serial"])]
+#[ORM\Index(columns: ["entities_id"])]
+#[ORM\Index(columns: ["is_recursive"])]
+#[ORM\Index(columns: ["busID"])]
+#[ORM\Index(columns: ["itemtype", "items_id"])]
+#[ORM\Index(columns: ["otherserial"])]
+#[ORM\Index(columns: ["locations_id"])]
+#[ORM\Index(columns: ["states_id"])]
+class ItemDeviceMemory
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private $id;
+
+    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    private $items_id;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private $itemtype;
+
+    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    private $devicememories_id;
+
+    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    private $size;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private $serial;
+
+    #[ORM\Column(type: "boolean", options: ["default" => false])]
+    private $is_deleted;
+
+    #[ORM\Column(type: "boolean", options: ["default" => false])]
+    private $is_dynamic;
+
+    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    private $entities_id;
+
+    #[ORM\Column(type: "boolean", options: ["default" => false])]
+    private $is_recursive;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private $busID;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private $otherserial;
+
+    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    private $locations_id;
+
+    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    private $states_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getItemsId(): ?int
+    {
+        return $this->items_id;
+    }
+
+    public function setItemsId(?int $items_id): self
+    {
+        $this->items_id = $items_id;
+
+        return $this;
+    }
+
+    public function getItemtype(): ?string
+    {
+        return $this->itemtype;
+    }
+
+    public function setItemtype(?string $itemtype): self
+    {
+        $this->itemtype = $itemtype;
+
+        return $this;
+    }
+
+    public function getDevicememoriesId(): ?int
+    {
+        return $this->devicememories_id;
+    }
+
+    public function setDevicememoriesId(?int $devicememories_id): self
+    {
+        $this->devicememories_id = $devicememories_id;
+
+        return $this;
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->size;
+    }
+
+    public function setSize(?int $size): self
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    public function getSerial(): ?string
+    {
+        return $this->serial;
+    }
+
+    public function setSerial(?string $serial): self
+    {
+        $this->serial = $serial;
+
+        return $this;
+    }
+
+    public function getIsDeleted(): ?bool
+    {
+        return $this->is_deleted;
+    }
+
+    public function setIsDeleted(?bool $is_deleted): self
+    {
+        $this->is_deleted = $is_deleted;
+
+        return $this;
+    }
+
+    public function getIsDynamic(): ?bool
+    {
+        return $this->is_dynamic;
+    }
+
+    public function setIsDynamic(?bool $is_dynamic): self
+    {
+        $this->is_dynamic = $is_dynamic;
+
+        return $this;
+    }
+
+    public function getEntitiesId(): ?int
+    {
+        return $this->entities_id;
+    }
+
+    public function setEntitiesId(?int $entities_id): self
+    {
+        $this->entities_id = $entities_id;
+
+        return $this;
+    }
+
+    public function getIsRecursive(): ?bool
+    {
+        return $this->is_recursive;
+    }
+
+    public function setIsRecursive(?bool $is_recursive): self
+    {
+        $this->is_recursive = $is_recursive;
+
+        return $this;
+    }
+
+    public function getBusID(): ?string
+    {
+        return $this->busID;
+    }
+
+    public function setBusID(?string $busID): self
+    {
+        $this->busID = $busID;
+
+        return $this;
+    }
+
+    public function getOtherserial(): ?string
+    {
+        return $this->otherserial;
+    }
+
+    public function setOtherserial(?string $otherserial): self
+    {
+        $this->otherserial = $otherserial;
+
+        return $this;
+    }
+
+    public function getLocationsId(): ?int
+    {
+        return $this->locations_id;
+    }
+
+    public function setLocationsId(?int $locations_id): self
+    {
+        $this->locations_id = $locations_id;
+
+        return $this;
+    }
+
+    public function getStatesId(): ?int
+    {
+        return $this->states_id;
+    }
+
+    public function setStatesId(?int $states_id): self
+    {
+        $this->states_id = $states_id;
+
+        return $this;
+    }
+}
