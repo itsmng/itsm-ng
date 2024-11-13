06889be18ea98044ea3495e4834bb75f189ef79e
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-13 15:27:52 +0100

    feat: add Knowbaseitemcategory entity

---
+++ src/Domain/Entities/Knowbaseitemcategory.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "glpi_knowbaseitemcategories")]
+#[ORM\UniqueConstraint(columns: ["entities_id", "knowbaseitemcategories_id", "name"])]
+#[ORM\Index(columns: ["name"])]
+#[ORM\Index(columns: ["entities_id"])]
+#[ORM\Index(columns: ["is_recursive"])]
+#[ORM\Index(columns: ["date_mod"])]
+#[ORM\Index(columns: ["date_creation"])]
+class Knowbaseitemcategory
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private $id;
+
+    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    private $entities_id;
+
+    #[ORM\Column(type: "boolean", options: ["default" => 0])]
+    private $is_recursive;
+
+    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    private $knowbaseitemcategories_id;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: "text", nullable: true, length: 65535)]
+    private $completename;
+
+    #[ORM\Column(type: "text", nullable: true, length: 65535)]
+    private $comment;
+
+    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    private $level;
+
+    #[ORM\Column(type: "text", nullable: true)]
+    private $sons_cache;
+
+    #[ORM\Column(type: "text", nullable: true)]
+    private $ancestors_cache;
+
+    #[ORM\Column(type: "datetime", nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: "datetime", nullable: true)]
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEntitiesId(): ?int
+    {
+        return $this->entities_id;
+    }
+
+    public function setEntitiesId(int $entities_id): self
+    {
+        $this->entities_id = $entities_id;
+
+        return $this;
+    }
+
+    public function getIsRecursive(): ?bool
+    {
+        return $this->is_recursive;
+    }
+
+    public function setIsRecursive(bool $is_recursive): self
+    {
+        $this->is_recursive = $is_recursive;
+
+        return $this;
+    }
+
+    public function getKnowbaseitemcategoriesId(): ?int
+    {
+        return $this->knowbaseitemcategories_id;
+    }
+
+    public function setKnowbaseitemcategoriesId(int $knowbaseitemcategories_id): self
+    {
+        $this->knowbaseitemcategories_id = $knowbaseitemcategories_id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCompletename(): ?string
+    {
+        return $this->completename;
+    }
+
+    public function setCompletename(string $completename): self
+    {
+        $this->completename = $completename;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getSonsCache(): ?string
+    {
+        return $this->sons_cache;
+    }
+
+    public function setSonsCache(string $sons_cache): self
+    {
+        $this->sons_cache = $sons_cache;
+
+        return $this;
+    }
+
+    public function getAncestorsCache(): ?string
+    {
+        return $this->ancestors_cache;
+    }
+
+    public function setAncestorsCache(string $ancestors_cache): self
+    {
+        $this->ancestors_cache = $ancestors_cache;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+}
