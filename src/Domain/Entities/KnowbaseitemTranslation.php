6f06c5835055c814005f16854be930d9745540d0
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-11-13 15:50:04 +0100

    feat: add KnowbaseitemTranslation entity

---
+++ src/Domain/Entities/KnowbaseitemTranslation.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_knowbaseitemtranslations')]
+#[ORM\Index(columns: ['knowbaseitems_id', 'language'])]
+#[ORM\Index(columns: ['users_id'])]
+#[ORM\Index(columns: ['name', 'answer'], flags: ['FULLTEXT'])]
+#[ORM\Index(columns: ['name'], flags: ['FULLTEXT'])]
+#[ORM\Index(columns: ['answer'], flags: ['FULLTEXT'])]
+class KnowbaseitemTranslation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $knowbaseitems_id;
+
+    #[ORM\Column(type: 'string', length: 10, nullable: true)]
+    private $language;
+
+    #[ORM\Column(type: 'text', nullable: true, length: 65535)]
+    private $name;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $answer;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $users_id;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getKnowbaseitemsId(): ?int
+    {
+        return $this->knowbaseitems_id;
+    }
+
+    public function setKnowbaseitemsId(int $knowbaseitems_id): self
+    {
+        $this->knowbaseitems_id = $knowbaseitems_id;
+
+        return $this;
+    }
+
+    public function getLanguage(): ?string
+    {
+        return $this->language;
+    }
+
+    public function setLanguage(?string $language): self
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAnswer(): ?string
+    {
+        return $this->answer;
+    }
+
+    public function setAnswer(?string $answer): self
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+
+    public function getUsersId(): ?int
+    {
+        return $this->users_id;
+    }
+
+    public function setUsersId(int $users_id): self
+    {
+        $this->users_id = $users_id;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+}
