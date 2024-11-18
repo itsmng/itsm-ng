dadd73a48320023590f9ab1505b2a72a9b7710ee
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-18 16:31:31 +0100

    feat: OidcUser Entity

---
+++ src/Domain/Entities/OidcUser.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_oidc_users')]
+class OidcUser
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]   
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default'=> 0])]
+    private $user_id;
+    
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $update;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?int $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getUpdate(): ?bool
+    {
+        return $this->update;
+    }
+
+    public function setUpdate(?bool $update): self
+    {
+        $this->update = $update;
+
+        return $this;
+    }
+
+}
