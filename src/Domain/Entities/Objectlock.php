df1d3c3eec3e2c2393cd4049f119e674865f048f
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-18 14:17:33 +0100

    feat: Objectlock Entity

---
+++ src/Domain/Entities/Objectlock.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_objectlocks')]
+#[ORM\UniqueConstraint(name: 'itemtype_items_id', columns: ['itemtype', 'items_id'])]
+class Objectlock
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 100, options: ['comment' => 'Type of locked object'])]
+    private $itemtype;
+    
+    #[ORM\Column(type: 'integer', options: ['comment' => 'RELATION to various tables, according to itemtype (ID)'])]
+    private $items_id;
+    
+    #[ORM\Column(type: 'integer', options: ['comment' => 'id of the locker'])]
+    private $users_id;
+    
+    #[ORM\Column(type: 'datetime', options: ['default' => 'CURRENT_TIMESTAMP', 'comment' => 'Timestamp of the lock'])]
+    private $date_mod;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getItemtype(): ?string
+    {
+        return $this->itemtype;
+    }
+
+    public function setItemtype(?string $itemtype): self
+    {
+        $this->itemtype = $itemtype;
+
+        return $this;
+    }
+
+    public function getItemsId(): ?int
+    {
+        return $this->items_id;
+    }
+
+    public function setItemsId(?int $items_id): self
+    {
+        $this->items_id = $items_id;
+
+        return $this;
+    }
+
+    public function getUsersId(): ?int
+    {
+        return $this->users_id;
+    }
+
+    public function setUsersId(?int $users_id): self
+    {
+        $this->users_id = $users_id;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+}
