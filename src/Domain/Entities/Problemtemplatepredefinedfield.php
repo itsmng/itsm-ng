e6075213226079a83c5db9fc000dacfc7e41065b
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-19 16:58:53 +0100

    feat: Problemtemplatepredefinedfield Entity

---
+++ src/Domain/Entities/Problemtemplatepredefinedfield.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_problemtemplatepredefinedfields')]
+#[ORM\Index(name: "problemtemplates_id", columns: ["problemtemplates_id"])]
+class Problemtemplatepredefinedfield
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $problemtemplates_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $num;
+
+    #[ORM\Column(type: 'text', length: 65535, nullable: true)]
+    private $value;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProblemtemplatesId(): ?int
+    {
+        return $this->problemtemplates_id;
+    }
+
+
+    public function setProblemtemplatesId(?int $problemtemplates_id): self
+    {
+        $this->problemtemplates_id = $problemtemplates_id;
+
+        return $this;
+    }
+
+    public function getNum(): ?int
+    {
+        return $this->num;
+    }
+
+
+    public function setNum(?int $num): self
+    {
+        $this->num = $num;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+
+    public function setValue(?string $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+}
