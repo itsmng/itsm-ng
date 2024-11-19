c23804f4332741a1c3af36abe17570eee9af5c12
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-19 10:07:35 +0100

    feat: Operatingsystemkernelversion Entity

---
+++ src/Domain/Entities/Operatingsystemkernelversion.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_operatingsystemkernelversions')]
+#[ORM\Index(name: 'name', columns: ['name'])]
+#[ORM\Index(name: 'operatingsystemkernels_id', columns: ['operatingsystemkernels_id'])]
+class Operatingsystemkernelversion
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $operatingsystemkernels_id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: 'text', length: 65535, nullable: true)]
+    private $comment;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOperatingsystemkernelsId(): ?int
+    {
+        return $this->operatingsystemkernels_id;
+    }
+
+    public function setOperatingsystemkernelsId(?int $operatingsystemkernels_id): self
+    {
+        $this->operatingsystemkernels_id = $operatingsystemkernels_id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(?\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(?\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+
+}
