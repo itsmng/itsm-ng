2c18a80e2203f7c19da81491b7317c402bb48eb3
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-20 13:52:49 +0100

    feat: Solutiontemplate Entity

---
+++ src/Domain/Entities/Solutiontemplate.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "glpi_solutiontemplates")]
+#[ORM\Index(name: "name", columns: ["name"])]
+#[ORM\Index(name: "is_recursive", columns: ["is_recursive"])]
+#[ORM\Index(name: "solutiontypes_id", columns: ["solutiontypes_id"])]
+#[ORM\Index(name: "entities_id", columns: ["entities_id"])]
+#[ORM\Index(name: "date_mod", columns: ["date_mod"])]
+#[ORM\Index(name: "date_creation", columns: ["date_creation"])]
+Class Solutiontemplate
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $entities_id;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_recursive;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: 'text', length: 65535, nullable: true)]
+    private $content;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $solutiontypes_id;
+
+    #[ORM\Column(type: 'text', length: 65535, nullable: true)]
+    private $comment;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEntitiesId(): ?int
+    {
+        return $this->entities_id;
+    }
+
+    public function setEntitiesId(?int $entities_id): self
+    {
+        $this->entities_id = $entities_id;
+
+        return $this;
+    }
+
+    public function getIsRecursive(): ?bool
+    {
+        return $this->is_recursive;
+    }
+
+    public function setIsRecursive(?bool $is_recursive): self
+    {
+        $this->is_recursive = $is_recursive;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getSolutiontypesId(): ?int
+    {
+        return $this->solutiontypes_id;
+    }
+
+    public function setSolutiontypesId(?int $solutiontypes_id): self
+    {
+        $this->solutiontypes_id = $solutiontypes_id;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTime
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(?\DateTime $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTime
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(?\DateTime $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+
+}
