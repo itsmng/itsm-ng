403b8f3bff1c1930ac03b24fb67de1617964312a
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-20 13:58:05 +0100

    feat: Specialstatus Entity

---
+++ src/Domain/Entities/Specialstatus.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "glpi_specialstatuses")]
+Class Specialstatus
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 1])]
+    private $weight;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 1])]
+    private $is_active;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $color;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getWeight(): ?int
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(?int $weight): self
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->is_active;
+    }
+
+    public function setIsActive(?bool $is_active): self
+    {
+        $this->is_active = $is_active;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+}
