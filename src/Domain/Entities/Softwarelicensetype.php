381271fc9aed13b2fff6ab014e9f7f81a5961709
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-20 13:39:51 +0100

    feat: Softwarelicensetype Entity

---
+++ src/Domain/Entities/Softwarelicensetype.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "glpi_softwarelicensetypes")]
+#[ORM\Index(name: "name", columns: ["name"])]
+#[ORM\Index(name: "date_mod", columns: ["date_mod"])]
+#[ORM\Index(name: "date_creation", columns: ["date_creation"])]
+#[ORM\Index(name: "softwarelicensetypes_id", columns: ["softwarelicensetypes_id"])]
+Class Softwarelicensetype
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: 'text', length: 65535, nullable: true)]
+    private $comment;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_creation;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $softwarelicensetypes_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $level;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $ancestors_cache;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $sons_cache;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $entities_id;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_recursive;
+
+    #[ORM\Column(type: 'text', length: 65535, nullable: true)]
+    private $completename;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTime
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(?\DateTime $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTime
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(?\DateTime $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+
+    public function getSoftwarelicensetypesId(): ?int
+    {
+        return $this->softwarelicensetypes_id;
+    }
+
+    public function setSoftwarelicensetypesId(?int $softwarelicensetypes_id): self
+    {
+        $this->softwarelicensetypes_id = $softwarelicensetypes_id;
+
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(?int $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getAncestorsCache(): ?string
+    {
+        return $this->ancestors_cache;
+    }
+
+    public function setAncestorsCache(?string $ancestors_cache): self
+    {
+        $this->ancestors_cache = $ancestors_cache;
+
+        return $this;
+    }
+
+    public function getSonsCache(): ?string
+    {
+        return $this->sons_cache;
+    }
+
+    public function setSonsCache(?string $sons_cache): self
+    {
+        $this->sons_cache = $sons_cache;
+
+        return $this;
+    }
+
+    public function getEntitiesId(): ?int
+    {
+        return $this->entities_id;
+    }
+
+    public function setEntitiesId(?int $entities_id): self
+    {
+        $this->entities_id = $entities_id;
+
+        return $this;
+    }
+
+    public function getIsRecursive(): ?bool
+    {
+        return $this->is_recursive;
+    }
+
+    public function setIsRecursive(?bool $is_recursive): self
+    {
+        $this->is_recursive = $is_recursive;
+
+        return $this;
+    }
+
+    public function getCompletename(): ?string
+    {
+        return $this->completename;
+    }
+
+    public function setCompletename(?string $completename): self
+    {
+        $this->completename = $completename;
+
+        return $this;
+    }
+
+}
