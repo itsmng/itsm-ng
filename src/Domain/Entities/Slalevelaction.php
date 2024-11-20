8ff0a78e5159ce996648e580cb64507c55e8a5d5
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-20 11:56:27 +0100

    feat: Slalevelaction Entity

---
+++ src/Domain/Entities/Slalevelaction.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'glpi_slalevelactions')]
+#[ORM\Index(name: "slalevels_id", columns: ["slalevels_id"])]
+class Slalevelaction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $slalevels_id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $action_type;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $field;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $value;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSlalevelsId(): ?int
+    {
+        return $this->slalevels_id;
+    }
+
+    public function setSlalevelsId(int $slalevels_id): self
+    {
+        $this->slalevels_id = $slalevels_id;
+
+        return $this;
+    }
+
+    public function getActionType(): ?string
+    {
+        return $this->action_type;
+    }
+
+    public function setActionType(string $action_type): self
+    {
+        $this->action_type = $action_type;
+
+        return $this;
+    }
+
+    public function getField(): ?string
+    {
+        return $this->field;
+    }
+
+    public function setField(string $field): self
+    {
+        $this->field = $field;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }       
+
+}   
