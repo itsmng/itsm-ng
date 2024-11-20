18746e8eeac28895aff6b915b584a1f187522332
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-20 15:17:40 +0100

    feat: Tickettask Entity

---
+++ src/Domain/Entities/Tickettask.php
@@ -0,0 +1,322 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;    
+
+#[ORM\Entity]
+#[ORM\Table(name: "glpi_tickettasks")]
+#[ORM\UniqueConstraint(name: "uuid", columns: ["uuid"])]
+#[ORM\Index(name: "date", columns: ["date"])]
+#[ORM\Index(name: "date_mod", columns: ["date_mod"])]
+#[ORM\Index(name: "date_creation", columns: ["date_creation"])]
+#[ORM\Index(name: "users_id", columns: ["users_id"])]
+#[ORM\Index(name: "users_id_editor", columns: ["users_id_editor"])]
+#[ORM\Index(name: "tickets_id", columns: ["tickets_id"])]
+#[ORM\Index(name: "is_private", columns: ["is_private"])]
+#[ORM\Index(name: "taskcategories_id", columns: ["taskcategories_id"])]
+#[ORM\Index(name: "state", columns: ["state"])]
+#[ORM\Index(name: "users_id_tech", columns: ["users_id_tech"])]
+#[ORM\Index(name: "groups_id_tech", columns: ["groups_id_tech"])]
+#[ORM\Index(name: "begin", columns: ["begin"])]
+#[ORM\Index(name: "end", columns: ["end"])]
+#[ORM\Index(name: "tasktemplates_id", columns: ["tasktemplates_id"])]
+#[ORM\Index(name: "sourceitems_id", columns: ["sourceitems_id"])]
+Class TicketTask
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $uuid;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $tickets_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $taskcategories_id;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $users_id;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $users_id_editor;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $content;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $is_private;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $actiontime;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $begin;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $end;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 1])]
+    private $state;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $users_id_tech;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $groups_id_tech;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_creation;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $tasktemplates_id;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private $timeline_position;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $sourceitems_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUuid(): ?string
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid(?string $uuid): self
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    public function getTicketsId(): ?int
+    {
+        return $this->tickets_id;
+    }
+
+    public function setTicketsId(?int $tickets_id): self
+    {
+        $this->tickets_id = $tickets_id;
+
+        return $this;
+    }
+
+    public function getTaskcategoriesId(): ?int
+    {
+        return $this->taskcategories_id;
+    }
+
+    public function setTaskcategoriesId(?int $taskcategories_id): self
+    {
+        $this->taskcategories_id = $taskcategories_id;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTime
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTime $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getUsersId(): ?int
+    {
+        return $this->users_id;
+    }
+
+    public function setUsersId(?int $users_id): self
+    {
+        $this->users_id = $users_id;
+
+        return $this;
+    }
+
+    public function getUsersIdEditor(): ?int
+    {
+        return $this->users_id_editor;
+    }
+
+    public function setUsersIdEditor(?int $users_id_editor): self
+    {
+        $this->users_id_editor = $users_id_editor;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getIsPrivate(): ?bool
+    {
+        return $this->is_private;
+    }
+
+    public function setIsPrivate(?bool $is_private): self
+    {
+        $this->is_private = $is_private;
+
+        return $this;
+    }
+
+    public function getActiontime(): ?int
+    {
+        return $this->actiontime;
+    }
+
+    public function setActiontime(?int $actiontime): self
+    {
+        $this->actiontime = $actiontime;
+
+        return $this;
+    }
+
+    public function getBegin(): ?\DateTime
+    {
+        return $this->begin;
+    }
+
+    public function setBegin(?\DateTime $begin): self
+    {
+        $this->begin = $begin;
+
+        return $this;
+    }
+
+    public function getEnd(): ?\DateTime
+    {
+        return $this->end;
+    }
+
+    public function setEnd(?\DateTime $end): self
+    {
+        $this->end = $end;
+
+        return $this;
+    }
+
+    public function getState(): ?int
+    {
+        return $this->state;
+    }
+
+    public function setState(?int $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getUsersIdTech(): ?int
+    {
+        return $this->users_id_tech;
+    }
+
+    public function setUsersIdTech(?int $users_id_tech): self
+    {
+        $this->users_id_tech = $users_id_tech;
+
+        return $this;
+    }
+
+    public function getGroupsIdTech(): ?int
+    {
+        return $this->groups_id_tech;
+    }
+
+    public function setGroupsIdTech(?int $groups_id_tech): self
+    {
+        $this->groups_id_tech = $groups_id_tech;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTime
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(?\DateTime $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTime
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(?\DateTime $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+
+    public function getTasktemplatesId(): ?int
+    {
+        return $this->tasktemplates_id;
+    }
+
+    public function setTasktemplatesId(?int $tasktemplates_id): self
+    {
+        $this->tasktemplates_id = $tasktemplates_id;
+
+        return $this;
+    }
+
+    public function getTimelinePosition(): ?bool
+    {
+        return $this->timeline_position;
+    }
+
+    public function setTimelinePosition(?bool $timeline_position): self
+    {
+        $this->timeline_position = $timeline_position;
+
+        return $this;
+    }
+
+    public function getSourceitemsId(): ?int
+    {
+        return $this->sourceitems_id;
+    }
+
+    public function setSourceitemsId(?int $sourceitems_id): self
+    {
+        $this->sourceitems_id = $sourceitems_id;
+
+        return $this;
+    }
+
+}
