467022c9fb3baf581d5311f88ac108edc7776f53
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-20 13:21:41 +0100

    feat: Softwarecategory Entity

---
+++ src/Domain/Entities/Softwarecategory.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "glpi_softwarecategories")]
+#[ORM\Index(name: "softwarecategories_id", columns: ["softwarecategories_id"])]
+Class Softwarecategory
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: 'text', length: 65535, nullable: true)]
+    private $comment;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $softwarecategories_id;
+
+    #[ORM\Column(type: 'text', length: 65535, nullable: true)]
+    private $completename;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $level;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $ancestors_cache;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $sons_cache;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }    
+    
+    public function getSoftwarecategoriesId(): ?int
+    {
+        return $this->softwarecategories_id;
+    }
+
+    public function setSoftwarecategoriesId(?int $softwarecategories_id): self
+    {
+        $this->softwarecategories_id = $softwarecategories_id;
+
+        return $this;
+    }
+
+    public function getCompletename(): ?string
+    {
+        return $this->completename;
+    }
+
+    public function setCompletename(?string $completename): self
+    {
+        $this->completename = $completename;
+
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(?int $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getAncestorsCache(): ?string
+    {
+        return $this->ancestors_cache;
+    }
+
+    public function setAncestorsCache(?string $ancestors_cache): self
+    {
+        $this->ancestors_cache = $ancestors_cache;
+
+        return $this;
+    }
+
+    public function getSonsCache(): ?string
+    {
+        return $this->sons_cache;
+    }
+
+    public function setSonsCache(?string $sons_cache): self
+    {
+        $this->sons_cache = $sons_cache;
+
+        return $this;
+    }
+}
