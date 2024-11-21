634a5d3b32b94c1e47e0501c4951f471aab1be23
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-21 09:20:58 +0100

    feat: Vobject Entity

---
+++ src/Domain/Entities/Vobject.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Itsmng\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "glpi_vobjects")]
+#[ORM\UniqueConstraint(name: "itemtype_items_id", columns: ["itemtype", "items_id"])]
+#[ORM\Index(name: "itemtype_items_id", columns: ["itemtype", "items_id"])]
+#[ORM\Index(name: "date_mod", columns: ["date_mod"])]
+#[ORM\Index(name: "date_creation", columns: ["date_creation"])]
+class Vobject
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 100, nullable: true)]
+    private $itemtype;
+
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    private $items_id;
+
+    #[ORM\Column(type: 'text', length: 65535, nullable: true)]
+    private $data;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_mod;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getItemtype(): ?string
+    {
+        return $this->itemtype;
+    }
+
+    public function setItemtype(?string $itemtype): self
+    {
+        $this->itemtype = $itemtype;
+
+        return $this;
+    }
+
+    public function getItemsId(): ?int
+    {
+        return $this->items_id;
+    }
+
+    public function setItemsId(int $items_id): self
+    {
+        $this->items_id = $items_id;
+
+        return $this;
+    }
+
+    public function getData(): ?string
+    {
+        return $this->data;
+    }
+
+    public function setData(?string $data): self
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    public function getDateMod(): ?\DateTimeInterface
+    {
+        return $this->date_mod;
+    }
+
+    public function setDateMod(\DateTimeInterface $date_mod): self
+    {
+        $this->date_mod = $date_mod;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+}
