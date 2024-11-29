3b1c348ebacd025df83225ee14b240c175c063af
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-29 10:14:00 +0100

    feat: DeviceProcessor entity relationships

--- src/Domain/Entities/DeviceProcessor.php
+++ src/Domain/Entities/DeviceProcessor.php
@@ -29,8 +29,12 @@ class DeviceProcessor
     #[ORM\Column(type: "text", nullable: true, length: 65535)]
     private $comment;
 
-    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    #[ORM\Column(type: "integer", name: 'manufacturers_id', options: ["default" => 0])]
     private $manufacturers_id;
+
+    #[ORM\ManyToOne(targetEntity: Manufacturer::class)]
+    #[ORM\JoinColumn(name: 'manufacturers_id', referencedColumnName: 'id', nullable: false)]
+    private ?Manufacturer $manufacturer;
 
     #[ORM\Column(type: "integer", options: ["default" => 0])]
     private $frequency_default;
@@ -41,14 +45,22 @@ class DeviceProcessor
     #[ORM\Column(type: "integer", nullable: true)]
     private $nbthreads_default;
 
-    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    #[ORM\Column(type: "integer", name: 'entities_id', options: ["default" => 0])]
     private $entities_id;
+
+    #[ORM\ManyToOne(targetEntity: Entity::class)]
+    #[ORM\JoinColumn(name: 'entities_id', referencedColumnName: 'id', nullable: false)]
+    private ?Entity $entity;
 
     #[ORM\Column(type: "boolean", options: ["default" => 0])]
     private $is_recursive;
 
     #[ORM\Column(type: "integer", nullable: true)]
     private $deviceprocessormodels_id;
+
+    #[ORM\ManyToOne(targetEntity: Deviceprocessormodel::class)]
+    #[ORM\JoinColumn(name: 'deviceprocessormodels_id', referencedColumnName: 'id', nullable: false)]
+    private ?Deviceprocessormodel $deviceprocessormodel;
 
     #[ORM\Column(type: "datetime", nullable: true)]
     private $date_mod;
@@ -208,6 +220,66 @@ class DeviceProcessor
     public function setDateCreation(\DateTimeInterface $date_creation): self
     {
         $this->date_creation = $date_creation;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of manufacturer
+     */ 
+    public function getManufacturer()
+    {
+        return $this->manufacturer;
+    }
+
+    /**
+     * Set the value of manufacturer
+     *
+     * @return  self
+     */ 
+    public function setManufacturer($manufacturer)
+    {
+        $this->manufacturer = $manufacturer;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of entity
+     */ 
+    public function getEntity()
+    {
+        return $this->entity;
+    }
+
+    /**
+     * Set the value of entity
+     *
+     * @return  self
+     */ 
+    public function setEntity($entity)
+    {
+        $this->entity = $entity;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of deviceprocessormodel
+     */ 
+    public function getDeviceprocessormodel()
+    {
+        return $this->deviceprocessormodel;
+    }
+
+    /**
+     * Set the value of deviceprocessormodel
+     *
+     * @return  self
+     */ 
+    public function setDeviceprocessormodel($deviceprocessormodel)
+    {
+        $this->deviceprocessormodel = $deviceprocessormodel;
 
         return $this;
     }
