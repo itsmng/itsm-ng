4d65144416498d17b54a9a1088a0290a35dee91a
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-11-29 11:21:32 +0100

    feat: DomainrecordType entity relationships

--- src/Domain/Entities/DomainrecordType.php
+++ src/Domain/Entities/DomainrecordType.php
@@ -17,8 +17,12 @@ class DomainrecordType
     #[ORM\Column(type: "string", length: 255, nullable: true)]
     private $name;
 
-    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    #[ORM\Column(type: "integer", name: "entities_id", options: ["default" => 0])]
     private $entities_id;
+
+    #[ORM\ManyToOne(targetEntity: Entity::class)]
+    #[ORM\JoinColumn(name: 'entities_id', referencedColumnName: 'id', nullable: false)]
+    private ?Entity $entity;
 
     #[ORM\Column(type: "boolean", options: ["default" => 0])]
     private $is_recursive;
@@ -75,6 +79,26 @@ class DomainrecordType
     public function setComment(?string $comment): self
     {
         $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of entity
+     */ 
+    public function getEntity()
+    {
+        return $this->entity;
+    }
+
+    /**
+     * Set the value of entity
+     *
+     * @return  self
+     */ 
+    public function setEntity($entity)
+    {
+        $this->entity = $entity;
 
         return $this;
     }
