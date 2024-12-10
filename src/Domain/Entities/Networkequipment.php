d2c04cbf5c6f3b07ba8a57e70bc63f5bc6551e32
Mathilde Rouland <mathilde.rouland@itsm-ng.com>
2024-12-10 14:17:31 +0100

    feat: Networkequipment entity relationships

--- src/Domain/Entities/Networkequipment.php
+++ src/Domain/Entities/Networkequipment.php
@@ -32,8 +32,9 @@ class Networkequipment
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[ORM\Column(type: 'integer', options: ['default' => 0])]
-    private $entities_id;
+    #[ORM\ManyToOne(targetEntity: Entity::class)]
+    #[ORM\JoinColumn(name: 'entities_id', referencedColumnName: 'id', nullable: true)]
+    private ?Entity $entity;
 
     #[ORM\Column(type: 'boolean', options: ['default' => 0])]
     private $is_recursive;
@@ -56,11 +57,13 @@ class Networkequipment
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private $contact_num;
 
-    #[ORM\Column(type: 'integer', options: ['default' => 0])]
-    private $users_id_tech;
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(name: 'users_id_tech', referencedColumnName: 'id', nullable: true)]
+    private ?User $userTech;
 
-    #[ORM\Column(type: 'integer', options: ['default' => 0])]
-    private $groups_id_tech;
+    #[ORM\ManyToOne(targetEntity: Group::class)]
+    #[ORM\JoinColumn(name: 'groups_id_tech', referencedColumnName: 'id', nullable: true)]
+    private ?Group $groupTech;
 
     #[ORM\Column(type: 'datetime', nullable: true)]
     private $date_mod;
@@ -68,20 +71,25 @@ class Networkequipment
     #[ORM\Column(type: 'text', nullable: true, length: 65535)]
     private $comment;
 
-    #[ORM\Column(type: 'integer', options: ['default' => 0])]
-    private $locations_id;
+    #[ORM\ManyToOne(targetEntity: Location::class)]
+    #[ORM\JoinColumn(name: 'locations_id', referencedColumnName: 'id', nullable: true)]
+    private ?Location $location;
 
-    #[ORM\Column(type: 'integer', options: ['default' => 0])]
-    private $networks_id;
+    #[ORM\ManyToOne(targetEntity: Network::class)]
+    #[ORM\JoinColumn(name: 'networks_id', referencedColumnName: 'id', nullable: true)]
+    private ?Network $network;
 
-    #[ORM\Column(type: 'integer', options: ['default' => 0])]
-    private $networkequipmenttypes_id;
+    #[ORM\ManyToOne(targetEntity: Networkequipmenttype::class)]
+    #[ORM\JoinColumn(name: 'networkequipmenttypes_id', referencedColumnName: 'id', nullable: true)]
+    private ?Networkequipmenttype $networkequipmenttype;
 
-    #[ORM\Column(type: 'integer', options: ['default' => 0])]
-    private $networkequipmentmodels_id;
+    #[ORM\ManyToOne(targetEntity: Networkequipmentmodel::class)]
+    #[ORM\JoinColumn(name: 'networkequipmentmodels_id', referencedColumnName: 'id', nullable: true)]
+    private ?Networkequipmentmodel $networkequipmentmodel;
 
-    #[ORM\Column(type: 'integer', options: ['default' => 0])]
-    private $manufacturers_id;
+    #[ORM\ManyToOne(targetEntity: Manufacturer::class)]
+    #[ORM\JoinColumn(name: 'manufacturers_id', referencedColumnName: 'id', nullable: true)]
+    private ?Manufacturer $manufacturer;
 
     #[ORM\Column(type: 'boolean', options: ['default' => 0])]
     private $is_deleted;
@@ -92,14 +100,17 @@ class Networkequipment
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private $template_name;
 
-    #[ORM\Column(type: 'integer', options: ['default' => 0])]
-    private $users_id;
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(name: 'users_id', referencedColumnName: 'id', nullable: true)]
+    private ?User $user;
 
-    #[ORM\Column(type: 'integer', options: ['default' => 0])]
-    private $groups_id;
+    #[ORM\ManyToOne(targetEntity: Group::class)]
+    #[ORM\JoinColumn(name: 'groups_id', referencedColumnName: 'id', nullable: true)]
+    private ?Group $group;
 
-    #[ORM\Column(type: 'integer', options: ['default' => 0])]
-    private $states_id;
+    #[ORM\ManyToOne(targetEntity: State::class)]
+    #[ORM\JoinColumn(name: 'states_id', referencedColumnName: 'id', nullable: true)]
+    private ?State $state;
 
     #[ORM\Column(type: 'decimal', precision: 20, scale: 4, options: ['default' => 0.0], nullable: true)]
     private $ticket_tco;
@@ -113,18 +124,6 @@ class Networkequipment
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getEntitiesId(): ?int
-    {
-        return $this->entities_id;
-    }
-
-    public function setEntitiesId(int $entities_id): self
-    {
-        $this->entities_id = $entities_id;
-
-        return $this;
     }
 
     public function getIsRecursive(): ?bool
@@ -211,30 +210,6 @@ class Networkequipment
         return $this;
     }
 
-    public function getUsersIdTech(): ?int
-    {
-        return $this->users_id_tech;
-    }
-
-    public function setUsersIdTech(int $users_id_tech): self
-    {
-        $this->users_id_tech = $users_id_tech;
-
-        return $this;
-    }
-
-    public function getGroupsIdTech(): ?int
-    {
-        return $this->groups_id_tech;
-    }
-
-    public function setGroupsIdTech(int $groups_id_tech): self
-    {
-        $this->groups_id_tech = $groups_id_tech;
-
-        return $this;
-    }
-
     public function getDateMod(): ?\DateTimeInterface
     {
         return $this->date_mod;
@@ -259,65 +234,6 @@ class Networkequipment
         return $this;
     }
 
-    public function getLocationsId(): ?int
-    {
-        return $this->locations_id;
-    }
-
-    public function setLocationsId(int $locations_id): self
-    {
-        $this->locations_id = $locations_id;
-
-        return $this;
-    }
-
-    public function getNetworksId(): ?int
-    {
-        return $this->networks_id;
-    }
-
-    public function setNetworksId(int $networks_id): self
-    {
-        $this->networks_id = $networks_id;
-
-        return $this;
-    }
-
-    public function getNetworkequipmenttypesId(): ?int
-    {
-        return $this->networkequipmenttypes_id;
-    }
-
-    public function setNetworkequipmenttypesId(int $networkequipmenttypes_id): self
-    {
-        $this->networkequipmenttypes_id = $networkequipmenttypes_id;
-
-        return $this;
-    }
-
-    public function getNetworkequipmentmodelsId(): ?int
-    {
-        return $this->networkequipmentmodels_id;
-    }
-
-    public function setNetworkequipmentmodelsId(int $networkequipmentmodels_id): self
-    {
-        $this->networkequipmentmodels_id = $networkequipmentmodels_id;
-
-        return $this;
-    }
-
-    public function getManufacturersId(): ?int
-    {
-        return $this->manufacturers_id;
-    }
-
-    public function setManufacturersId(int $manufacturers_id): self
-    {
-        $this->manufacturers_id = $manufacturers_id;
-
-        return $this;
-    }
 
     public function getIsDeleted(): ?bool
     {
@@ -355,41 +271,6 @@ class Networkequipment
         return $this;
     }
 
-    public function getUsersId(): ?int
-    {
-        return $this->users_id;
-    }
-
-    public function setUsersId(int $users_id): self
-    {
-        $this->users_id = $users_id;
-
-        return $this;
-    }
-
-    public function getGroupsId(): ?int
-    {
-        return $this->groups_id;
-    }
-
-    public function setGroupsId(int $groups_id): self
-    {
-        $this->groups_id = $groups_id;
-
-        return $this;
-    }
-
-    public function getStatesId(): ?int
-    {
-        return $this->states_id;
-    }
-
-    public function setStatesId(int $states_id): self
-    {
-        $this->states_id = $states_id;
-
-        return $this;
-    }
 
     public function getTicketTco(): ?float
     {
@@ -423,6 +304,226 @@ class Networkequipment
     public function setDateCreation(\DateTimeInterface $date_creation): self
     {
         $this->date_creation = $date_creation;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of entity
+     */ 
+    public function getEntity()
+    {
+        return $this->entity;
+    }
+
+    /**
+     * Set the value of entity
+     *
+     * @return  self
+     */ 
+    public function setEntity($entity)
+    {
+        $this->entity = $entity;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of userTech
+     */ 
+    public function getUserTech()
+    {
+        return $this->userTech;
+    }
+
+    /**
+     * Set the value of userTech
+     *
+     * @return  self
+     */ 
+    public function setUserTech($userTech)
+    {
+        $this->userTech = $userTech;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of groupTech
+     */ 
+    public function getGroupTech()
+    {
+        return $this->groupTech;
+    }
+
+    /**
+     * Set the value of groupTech
+     *
+     * @return  self
+     */ 
+    public function setGroupTech($groupTech)
+    {
+        $this->groupTech = $groupTech;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of location
+     */ 
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * Set the value of location
+     *
+     * @return  self
+     */ 
+    public function setLocation($location)
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of network
+     */ 
+    public function getNetwork()
+    {
+        return $this->network;
+    }
+
+    /**
+     * Set the value of network
+     *
+     * @return  self
+     */ 
+    public function setNetwork($network)
+    {
+        $this->network = $network;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of networkequipmenttype
+     */ 
+    public function getNetworkequipmenttype()
+    {
+        return $this->networkequipmenttype;
+    }
+
+    /**
+     * Set the value of networkequipmenttype
+     *
+     * @return  self
+     */ 
+    public function setNetworkequipmenttype($networkequipmenttype)
+    {
+        $this->networkequipmenttype = $networkequipmenttype;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of networkequipmentmodel
+     */ 
+    public function getNetworkequipmentmodel()
+    {
+        return $this->networkequipmentmodel;
+    }
+
+    /**
+     * Set the value of networkequipmentmodel
+     *
+     * @return  self
+     */ 
+    public function setNetworkequipmentmodel($networkequipmentmodel)
+    {
+        $this->networkequipmentmodel = $networkequipmentmodel;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of manufacturer
+     */ 
+    public function getManufacturer()
+    {
+        return $this->manufacturer;
+    }
+
+    /**
+     * Set the value of manufacturer
+     *
+     * @return  self
+     */ 
+    public function setManufacturer($manufacturer)
+    {
+        $this->manufacturer = $manufacturer;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user
+     */ 
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set the value of user
+     *
+     * @return  self
+     */ 
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of group
+     */ 
+    public function getGroup()
+    {
+        return $this->group;
+    }
+
+    /**
+     * Set the value of group
+     *
+     * @return  self
+     */ 
+    public function setGroup($group)
+    {
+        $this->group = $group;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of state
+     */ 
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * Set the value of state
+     *
+     * @return  self
+     */ 
+    public function setState($state)
+    {
+        $this->state = $state;
 
         return $this;
     }
