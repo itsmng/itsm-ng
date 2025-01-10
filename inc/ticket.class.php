341802ab249abfd42904960b664d386a26782011
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2025-01-10 11:48:01 +0100

    fix: added back contect to Pending status

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -4876,8 +4876,6 @@ class Ticket extends CommonITILObject
         $display_save_btn = (!array_key_exists('locked', $options) || !$options['locked'])
            && ($canupdate || $can_requester || $canpriority || $canassign || $canassigntome);
 
-        $rand = mt_rand();
-
         $formUrl = $this->getFormURL();
         $reopenLabel = __('Reopen');
         $form = [
