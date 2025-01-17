5ff1764c8e86bb36fb6c28bd25324a5215852b59
Ower <83206135+Lcanet2@users.noreply.github.com>
2025-01-17 15:52:50 +0100

    Update dropdownItilActors.php

--- ajax/dropdownItilActors.php
+++ ajax/dropdownItilActors.php
@@ -2,10 +2,9 @@
 
 /**
  * ---------------------------------------------------------------------
- * GLPI - Gestionnaire Libre de Parc Informatique
- * Copyright (C) 2015-2022 Teclib' and contributors.
+ * Copyright (C) 2022 ITSM-NG and contributors.
  *
- * http://glpi-project.org
+ * http://www.itsm-ng.org
  *
  * based on GLPI - Gestionnaire Libre de Parc Informatique
  * Copyright (C) 2003-2014 by the INDEPNET Development Team.
@@ -14,20 +13,20 @@
  *
  * LICENSE
  *
- * This file is part of GLPI.
+ * This file is part of ITSM-NG.
  *
- * GLPI is free software; you can redistribute it and/or modify
+ * ITSM-NG is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * GLPI is distributed in the hope that it will be useful,
+ * ITSM-NG is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
+ * along with ITSM-NG. If not, see <http://www.gnu.org/licenses/>.
  * ---------------------------------------------------------------------
  */
 
